d6eadc6712e08b3c1d735921649b1e710962c51f
Kyle Bartz <mail@kyle.fail>
2023-05-06 06:09:13 +0000

    Added storybook

---
+++ ui/packages/web/.storybook/preview.js
@@ -0,0 +1,19 @@
+import "../src/index.css";
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    backgrounds: {
+      default: "dark",
+    },
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
