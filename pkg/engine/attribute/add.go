ce506c9640f088243f11c3ae3b3a2866e30efd07
Kyle Bartz <mail@kyle.fail>
2023-05-22 01:02:06 +0000

    Refactored attribute package

---
+++ pkg/engine/attribute/add.go
@@ -0,0 +1,34 @@
+package attribute
+
+import (
+	"fmt"
+
+	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/key"
+)
+
+// TODO: should have AddCharacter & AddEnemy w/ specific proto defs as input?
+// TODO: starting energy
+type BaseStats struct {
+	Stats     info.PropMap
+	MaxEnergy float64
+	MaxStance float64
+}
+
+func (s *Service) AddTarget(target key.TargetID, base BaseStats) error {
+	if _, dup := s.targets[target]; dup {
+		return fmt.Errorf("target base stats already registered: %v", target)
+	}
+	if base.Stats == nil {
+		base.Stats = info.NewPropMap()
+	}
+
+	s.targets[target] = &info.Attributes{
+		BaseStats: base.Stats,
+		MaxStance: base.MaxStance,
+		Stance:    base.MaxStance,
+		MaxEnergy: base.MaxEnergy,
+		Energy:    base.MaxEnergy,
+	}
+	return nil
+}
--- pkg/engine/attribute/attribute.go
+++ pkg/engine/attribute/attribute.go
@@ -3,50 +3,36 @@
 package attribute
 
 import (
-	"fmt"
-
+	"github.com/simimpact/srsim/pkg/engine"
+	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/modifier"
 	"github.com/simimpact/srsim/pkg/key"
-	"github.com/simimpact/srsim/pkg/model"
 )
 
+// TODO: there should be an explicit base stats structure?
 type Service struct {
-	targetBaseStats map[key.TargetID][]float64
+	engine          engine.Engine
+	modifierManager *modifier.Manager
+	targets         map[key.TargetID]*info.Attributes
 }
 
-//TargetBaseAttrFunc shold return the base stats given a targetID
-type TargetBaseAttrFunc func(key.TargetID) []float64
-
-func New(targets []key.TargetID, baseFn TargetBaseAttrFunc) (*Service, error) {
-	s := &Service{
-		targetBaseStats: make(map[key.TargetID][]float64),
+func New(engine engine.Engine, modManager *modifier.Manager) *Service {
+	return &Service{
+		engine:          engine,
+		modifierManager: modManager,
+		targets:         make(map[key.TargetID]*info.Attributes),
 	}
-
-	for _, v := range targets {
-		base := baseFn(v)
-		if base == nil {
-			return nil, fmt.Errorf("baseFn did not provide base stats for target id %v", v)
-		}
-		//sanity check?
-		if len(base) != len(model.AttributeType_value) {
-			return nil, fmt.Errorf("baseFn provided an attribute slice with invalid len; expecting %v, got %v", len(model.AttributeType_value), len(base))
-		}
-
-		s.targetBaseStats[v] = base
-	}
-
-
-	return s, nil
 }
 
-
-func (s *Service) Stat(id key.TargetID, stat model.AttributeType) float64 {
-	v, ok := s.targetBaseStats[id]
-	if !ok {
-		return 0
+func (s *Service) Stats(target key.TargetID) *info.Stats {
+	mods := s.modifierManager.EvalModifiers(target)
+	attr := s.targets[target]
+	if attr == nil {
+		attr = &info.Attributes{}
 	}
-	//TODO: do we need this sanity check here?
-	if _, ok := model.AttributeType_name[int32(stat)]; !ok {
-		return 0
-	}
-	return v[stat]
+	return info.NewStats(target, *attr, mods)
 }
+
+// TODO: ChangeHP, return new HP (emit HPChangeEvent)
+// TODO: ChangeStance, return new Stance (emit StanceChangeEvent)
+// TODO: ChangeEnergy, return new Energy (emit EnergyChangeEvent)
