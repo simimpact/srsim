2327ebf1001402f3c427ab7d50d5b9803d961d84
Kisa <janbeier0@gmail.com>
2023-06-12 19:32:20 +0200

    Implemented Arlan (#12)
    * setup character template for arlan
    
    * implemented stats.go
    
    * implemented skill.go
    
    * implmemented attack.go
    
    * fixed attack hitratio
    
    * implemented ult.go
    
    * implemented talent.go
    
    * fixed talent.go
    
    * fixed ult EndAttack()
    
    * implemented technique.go
    
    * implemented trace.go
    
    * implemented eidolons
    
    * implemented arlan.go
    
    * Fixed e4

---
+++ internal/character/arlan/technique.go
@@ -0,0 +1,21 @@
+package arlan
+
+import (
+	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
+)
+
+func (c *char) Technique(target key.TargetID, state info.ActionState) {
+	c.engine.Attack(info.Attack{
+		Source:     c.id,
+		Targets:    c.engine.Enemies(),
+		DamageType: model.DamageType_THUNDER,
+		AttackType: model.AttackType_MAZE_NORMAL,
+		BaseDamage: info.DamageMap{
+			model.DamageFormula_BY_ATK: 0.8,
+		},
+		StanceDamage: 60.0,
+		EnergyGain:   0.0,
+	})
+}
