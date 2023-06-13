5d87b46b59f9789c1cf88819ac36068eaf121996
Kisa <janbeier0@gmail.com>
2023-06-13 15:42:38 +0200

    implemented attack.go

--- internal/character/bronya/attack.go
+++ internal/character/bronya/attack.go
@@ -3,8 +3,19 @@ package bronya
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
 )
 
 func (c *char) Attack(target key.TargetID, state info.ActionState) {
-
+	c.engine.Attack(info.Attack{
+		Source:     c.id,
+		Targets:    []key.TargetID{target},
+		DamageType: model.DamageType_WIND,
+		AttackType: model.AttackType_NORMAL,
+		BaseDamage: info.DamageMap{
+			model.DamageFormula_BY_ATK: atk[c.info.AbilityLevel.Attack],
+		},
+		StanceDamage: 30.0,
+		EnergyGain:   20.0,
+	})
 }
