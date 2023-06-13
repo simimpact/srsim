b685a8c9f9c32489ab55cf808694f357a6b63897
skippi <jbtcao@gmail.com>
2023-06-12 23:03:13 -0500

    refactor pela talent
    Some of the checks weren't needed and we needed to fix the debuff check
    logic.

--- internal/character/pela/talent.go
+++ internal/character/pela/talent.go
@@ -5,6 +5,7 @@ import (
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/engine/modifier"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
 )
 
 const (
@@ -15,7 +16,16 @@ const (
 func init() {
 	modifier.Register(Talent, modifier.Config{
 		Listeners: modifier.Listeners{
-			OnAfterAttack: talentAfterAttack,
+			OnAfterAttack: func(mod *modifier.ModifierInstance, e event.AttackEndEvent) {
+				char, _ := mod.Engine().CharacterInfo(mod.Owner())
+				for _, trg := range e.Targets {
+					if mod.Engine().ModifierCount(trg, model.StatusType_STATUS_DEBUFF) >= 1 {
+						mod.Engine().ModifyEnergy(mod.Owner(), talent[char.AbilityLevel.Talent-1])
+						return
+
+					}
+				}
+			},
 		},
 	})
 }
@@ -24,19 +34,5 @@ func (c *char) initTalent() {
 	c.engine.AddModifier(c.id, info.Modifier{
 		Name:   Talent,
 		Source: c.id,
-	})
-}
-
-func talentAfterAttack(mod *modifier.ModifierInstance, e event.AttackEndEvent) {
-	if mod.Engine().HasModifier(mod.Owner(), TalentCD) {
-		return
-	}
-	char, _ := mod.Engine().CharacterInfo(mod.Owner())
-	mod.Engine().ModifyEnergy(mod.Owner(), talent[char.AbilityLevel.Talent-1])
-	mod.Engine().AddModifier(mod.Owner(), info.Modifier{
-		Name:            TalentCD,
-		Source:          mod.Owner(),
-		Duration:        1,
-		TickImmediately: true,
 	})
 }
