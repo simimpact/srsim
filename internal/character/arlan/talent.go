a6b43d79cfc2bb56a21f3f376a2b617863b5f50b
connorhhuang <connorhhuang@gmail.com>
2023-06-15 22:44:18 +0800

    unoopsied arlan

--- internal/character/arlan/talent.go
+++ internal/character/arlan/talent.go
@@ -1,8 +1,10 @@
 package arlan
 
 import (
+	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/engine/modifier"
+	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
 )
@@ -17,12 +19,23 @@ type talentState struct {
 
 func init() {
 	modifier.Register(Talent, modifier.Config{
+		Stacking:   modifier.ReplaceBySource,
 		StatusType: model.StatusType_STATUS_BUFF,
 		Listeners: modifier.Listeners{
-			OnBeforeHitAll: talentBeforeHitAll,
-			OnAfterAction:  talentAfterAction,
+			// set bonus damage on combat start
+			OnAdd: func(mod *modifier.ModifierInstance) {
+				addedBonusDamage := (1 - mod.Engine().HPRatio(mod.Owner())) * mod.State().(talentState).maxBonusDamage
+				mod.SetProperty(prop.AllDamagePercent, addedBonusDamage)
+			},
+
+			// update bonus damage based on new HP
+			OnHPChange: func(mod *modifier.ModifierInstance, e event.HPChangeEvent) {
+				addedBonusDamage := (1 - mod.Engine().HPRatio(mod.Owner())) * mod.State().(talentState).maxBonusDamage
+				mod.SetProperty(prop.AllDamagePercent, addedBonusDamage)
+			},
 		},
 	})
+
 }
 
 func (c *char) addTalentMod() {
