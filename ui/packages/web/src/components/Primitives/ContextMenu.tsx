ace5b7503bbe668083b94ec2a6eb6a4891f4c652
Othi <mnpq.raven@gmail.com>
2023-06-17 23:58:34 +0700

    context menu

--- ui/packages/web/src/components/Primitives/ContextMenu.tsx
+++ ui/packages/web/src/components/Primitives/ContextMenu.tsx
@@ -1,9 +1,39 @@
 "use client";
 
 import * as ContextMenuPrimitive from "@radix-ui/react-context-menu";
+import { VariantProps, cva } from "class-variance-authority";
 import { Check, ChevronRight, Circle } from "lucide-react";
 import * as React from "react";
 import { cn } from "@/utils/classname";
+
+const subTriggerVariants = cva(
+  "flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground",
+  {
+    variants: { variant: { default: "" } },
+    defaultVariants: { variant: "default" },
+  }
+);
+const subContentVariants = cva(
+  "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md animate-in slide-in-from-left-1"
+);
+const contentVariants = cva(
+  "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md animate-in fade-in-80"
+);
+const itemVariants = cva(
+  "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+);
+const checkboxItemVariants = cva(
+  "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+);
+const radioItemVariants = cva(
+  "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+);
+
+const labelVariants = cva("px-2 py-1.5 text-sm font-semibold text-foreground");
+
+const separatorVariants = cva("-mx-1 my-1 h-px bg-border");
+
+const shortcutVariants = cva("ml-auto text-xs tracking-widest text-muted-foreground");
 
 const ContextMenu = ContextMenuPrimitive.Root;
 
@@ -20,14 +50,10 @@ const ContextMenuRadioGroup = ContextMenuPrimitive.RadioGroup;
 const ContextMenuSubTrigger = React.forwardRef<
   React.ElementRef<typeof ContextMenuPrimitive.SubTrigger>,
   SubTriggerProps
->(({ className, inset, children, ...props }, ref) => (
+>(({ className, inset, variant, children, ...props }, ref) => (
   <ContextMenuPrimitive.SubTrigger
     ref={ref}
-    className={cn(
-      "flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground",
-      inset && "pl-8",
-      className
-    )}
+    className={cn(subTriggerVariants({ variant, className }), inset && "pl-8")}
     {...props}
   >
     {children}
@@ -42,10 +68,7 @@ const ContextMenuSubContent = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ContextMenuPrimitive.SubContent
     ref={ref}
-    className={cn(
-      "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md animate-in slide-in-from-left-1",
-      className
-    )}
+    className={cn(subContentVariants({ className }))}
     {...props}
   />
 ));
@@ -58,10 +81,7 @@ const ContextMenuContent = React.forwardRef<
   <ContextMenuPrimitive.Portal>
     <ContextMenuPrimitive.Content
       ref={ref}
-      className={cn(
-        "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md animate-in fade-in-80",
-        className
-      )}
+      className={cn(contentVariants({ className }))}
       {...props}
     />
   </ContextMenuPrimitive.Portal>
@@ -74,11 +94,7 @@ const ContextMenuItem = React.forwardRef<
 >(({ className, inset, ...props }, ref) => (
   <ContextMenuPrimitive.Item
     ref={ref}
-    className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-      inset && "pl-8",
-      className
-    )}
+    className={cn(itemVariants({ className }), inset && "pl-8")}
     {...props}
   />
 ));
@@ -90,10 +106,7 @@ const ContextMenuCheckboxItem = React.forwardRef<
 >(({ className, children, checked, ...props }, ref) => (
   <ContextMenuPrimitive.CheckboxItem
     ref={ref}
-    className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-      className
-    )}
+    className={cn(checkboxItemVariants({ className }))}
     checked={checked}
     {...props}
   >
@@ -113,10 +126,7 @@ const ContextMenuRadioItem = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <ContextMenuPrimitive.RadioItem
     ref={ref}
-    className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-      className
-    )}
+    className={cn(radioItemVariants({ className }))}
     {...props}
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
@@ -135,7 +145,7 @@ const ContextMenuLabel = React.forwardRef<
 >(({ className, inset, ...props }, ref) => (
   <ContextMenuPrimitive.Label
     ref={ref}
-    className={cn("px-2 py-1.5 text-sm font-semibold text-foreground", inset && "pl-8", className)}
+    className={cn(labelVariants({ className }), inset && "pl-8")}
     {...props}
   />
 ));
@@ -147,50 +157,57 @@ const ContextMenuSeparator = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ContextMenuPrimitive.Separator
     ref={ref}
-    className={cn("-mx-1 my-1 h-px bg-border", className)}
+    className={cn(separatorVariants({ className }))}
     {...props}
   />
 ));
 ContextMenuSeparator.displayName = ContextMenuPrimitive.Separator.displayName;
 
 const ContextMenuShortcut = ({ className, ...props }: ShortcutProps) => {
-  return (
-    <span
-      className={cn("ml-auto text-xs tracking-widest text-muted-foreground", className)}
-      {...props}
-    />
-  );
+  return <span className={cn(shortcutVariants({ className }))} {...props} />;
 };
 ContextMenuShortcut.displayName = "ContextMenuShortcut";
 
 interface SubTriggerProps
-  extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.SubTrigger> {
+  extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.SubTrigger>,
+    VariantProps<typeof subTriggerVariants> {
   inset?: boolean;
 }
 interface SubContentProps
-  extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.SubContent> {}
+  extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.SubContent>,
+    VariantProps<typeof subContentVariants> {}
 
 interface ContentProps
-  extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.Content> {}
+  extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.Content>,
+    VariantProps<typeof contentVariants> {}
 
-interface ItemProps extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.Item> {
+interface ItemProps
+  extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.Item>,
+    VariantProps<typeof itemVariants> {
   inset?: boolean;
 }
 
 interface CheckboxItemProps
-  extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.CheckboxItem> {}
+  extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.CheckboxItem>,
+    VariantProps<typeof checkboxItemVariants> {}
 
 interface RadioItemProps
-  extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.RadioItem> {}
+  extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.RadioItem>,
+    VariantProps<typeof radioItemVariants> {}
 
-interface LabelProps extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.Label> {
+interface LabelProps
+  extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.Label>,
+    VariantProps<typeof labelVariants> {
   inset?: boolean;
 }
 
 interface SeparatorProps
-  extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.Separator> {}
+  extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.Separator>,
+    VariantProps<typeof separatorVariants> {}
 
-interface ShortcutProps extends React.HTMLAttributes<HTMLSpanElement> {}
+interface ShortcutProps
+  extends React.HTMLAttributes<HTMLSpanElement>,
+    VariantProps<typeof shortcutVariants> {}
 
 export {
   ContextMenu,
