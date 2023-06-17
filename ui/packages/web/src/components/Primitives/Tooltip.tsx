c6c61ce0f51027c3e5a0bfbb98dd98289cc51e73
Othi <mnpq.raven@gmail.com>
2023-06-18 00:32:23 +0700

    menubar + tooltip

--- ui/packages/web/src/components/Primitives/Tooltip.tsx
+++ ui/packages/web/src/components/Primitives/Tooltip.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import { cva } from "class-variance-authority";
 import * as React from "react";
 import { cn } from "@/utils/classname";
+
+const contentVariants = cva(
+  "z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-50 data-[side=bottom]:slide-in-from-top-1 data-[side=left]:slide-in-from-right-1 data-[side=right]:slide-in-from-left-1 data-[side=top]:slide-in-from-bottom-1"
+);
 
 const TooltipProvider = TooltipPrimitive.Provider;
 
@@ -17,10 +22,7 @@ const TooltipContent = React.forwardRef<
   <TooltipPrimitive.Content
     ref={ref}
     sideOffset={sideOffset}
-    className={cn(
-      "z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-50 data-[side=bottom]:slide-in-from-top-1 data-[side=left]:slide-in-from-right-1 data-[side=right]:slide-in-from-left-1 data-[side=top]:slide-in-from-bottom-1",
-      className
-    )}
+    className={cn(contentVariants({ className }))}
     {...props}
   />
 ));
