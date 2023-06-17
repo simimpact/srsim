cc5f57a0583e9eeb7e6abdab6bd3c3b81711852b
RyanOlder <ryanolder67@gmail.com>
2023-06-17 11:59:42 +0700

    Implement Cornucopia (LC)

--- internal/lightcone/abundance/cornucopia/cornucopia.go
+++ internal/lightcone/abundance/cornucopia/cornucopia.go
@@ -12,12 +12,8 @@ import (
 )
 
 const (
-	CornucopiaCheck key.Modifier = "cornucopia-check"
 	CornucopiaBuff  key.Modifier = "cornucopia-buff"
 )
-
-//When the wearer uses their Skill or Ultimate, 
-//their Outgoing Healing increases by 12%.
 
 func init() {
 	lightcone.Register(key.Cornucopia, lightcone.Config{
@@ -26,36 +22,39 @@ func init() {
 		Path:          model.Path_ABUNDANCE,
 		Promotions:    promotions,
 	})
-
-	modifier.Register(CornucopiaCheck, modifier.Config{
-		Listeners: modifier.Listeners{
-			OnBeforeAction: buffHealOnSkillUlt,
-		},
-	})
-
+	
 	modifier.Register(CornucopiaBuff, modifier.Config{
-		Stacking:   modifier.Unique,
-		StatusType: model.StatusType_STATUS_BUFF,
+		Listeners: modifier.Listeners{
+			OnBeforeAction: buffHealsOnSkillUlt,
+			OnAfterAction: removeHealBuff,
+		},
+		Stacking: modifier.Unique,
 	})
+
 }
 
+//When the wearer uses their Skill or Ultimate, their Outgoing Healing increases by 12%.
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
 	engine.AddModifier(owner, info.Modifier{
-		Name:   CornucopiaCheck,
+		Name:   CornucopiaBuff,
 		Source: owner,
-		State:  0.09 + 0.03*float64(lc.Imposition),
+		State:  0.09 + 0.03 * float64(lc.Imposition),
 	})
 }
 
-func buffHealOnSkillUlt(mod *modifier.ModifierInstance, e event.ActionEvent) {
-	amt := mod.State().(float64)
-
-	if (e.AttackType == model.AttackType_SKILL || e.AttackType == model.AttackType_ULT) {
-			mod.Engine().AddModifier(mod.Owner(), info.Modifier{
-				Name:     CornucopiaBuff,
-				Source:   mod.Owner(),
-				Duration: -1,
-				Stats:    info.PropMap{prop.HealBoost: amt},
-			})
-		}
+func buffHealsOnSkillUlt(mod *modifier.ModifierInstance, e event.ActionEvent) {
+	healAmt := mod.State().(float64)
+	switch e.AttackType {
+	case model.AttackType_SKILL, model.AttackType_ULT :
+		mod.Engine().AddModifier(mod.Owner(), info.Modifier{
+			Name:     CornucopiaBuff,
+			Source:   mod.Owner(),
+			Duration: -100,
+			Stats:    info.PropMap{prop.HealBoost: healAmt},
+		})
+	}
+}
+//is it neccessary to remove a permanent buff after each turn?
+func removeHealBuff(mod *modifier.ModifierInstance, e event.ActionEvent)  {
+	mod.Engine().RemoveModifier(mod.Owner(), mod.Name())
 }
