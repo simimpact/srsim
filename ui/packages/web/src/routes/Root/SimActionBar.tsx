7b7e606471c646a565b10b3134cd7f88eb88606b
Othi <mnpq.raven@gmail.com>
2023-06-17 13:55:56 +0700

    chore

--- ui/packages/web/src/routes/Root/SimActionBar.tsx
+++ ui/packages/web/src/routes/Root/SimActionBar.tsx
@@ -5,10 +5,9 @@ const SimActionBar = () => {
     <div className="flex flex-col justify-evenly self-center h-3/5 ml-8">
       <Button variant="success">Run</Button>
       <Button variant="destructive">Reset</Button>
-      <Button>Import</Button>
-      <Button>Export</Button>
-      <Button>Results</Button>
+      <Button>APL</Button>
       <Button>Debug</Button>
+      <Button>Im/export/Share group(radix grp)</Button>
     </div>
   );
 };
