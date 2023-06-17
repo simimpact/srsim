146cfedad6a0e67d94a9f46d3b72fe672cace8c7
Connor Kounthapanya <ckountha@gmail.com>
2023-06-17 12:53:11 -0600

    Update Return to Darkness

--- internal/lightcone/hunt/returntodarkness/returntodarkness.go
+++ internal/lightcone/hunt/returntodarkness/returntodarkness.go
@@ -17,7 +17,10 @@ const (
 	ReturntoDarkness key.Modifier = "return_to_darkness"
 )
 
-var wasTriggered bool = false
+type State struct {
+	chance       float64
+	wasTriggered bool
+}
 
 // Increases the wearer's CRIT Rate by 12/15/18/21/24%. After a CRIT Hit, there
 // is a 16/20/24/28/32% fixed chance to dispel 1 buff on the target enemy. This
@@ -41,6 +44,7 @@ func init() {
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
 	cr := 0.09 + 0.03*float64(lc.Imposition)
 	chance := 0.12 + 0.04*float64(lc.Imposition)
+	state := State{chance: chance, wasTriggered: false}
 
 	engine.AddModifier(owner, info.Modifier{
 		Name:   ReturntoDarkness,
@@ -48,21 +52,23 @@ func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
 		Stats: info.PropMap{
 			prop.CritChance: cr,
 		},
-		State: chance,
+		State: &state,
 	})
 }
 
 func onAfterHitAll(mod *modifier.ModifierInstance, e event.HitEndEvent) {
-	if e.IsCrit && !wasTriggered && rand.Float64() < mod.State().(float64) {
+	state := mod.State().(*State)
+
+	if e.IsCrit && !state.wasTriggered && rand.Float64() < state.chance {
 		mod.Engine().DispelStatus(e.Defender, info.Dispel{
 			Status: model.StatusType_STATUS_BUFF,
 			Order:  model.DispelOrder_LAST_ADDED,
 			Count:  1,
 		})
-		wasTriggered = true
+		state.wasTriggered = true
 	}
 }
 
 func onAfterAttack(mod *modifier.ModifierInstance, e event.AttackEndEvent) {
-	wasTriggered = false
+	mod.State().(*State).wasTriggered = false
 }
