22ec14f39f792042fe376a5c584646586e3101fc
Othi <mnpq.raven@gmail.com>
2023-06-17 23:58:18 +0700

    command

--- ui/packages/web/src/components/Primitives/Command.tsx
+++ ui/packages/web/src/components/Primitives/Command.tsx
@@ -18,17 +18,9 @@ const inputVariants = cva("flex items-center border-b px-3", {
       default:
         "flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
     },
-    search: {
-      default: "mr-2 h-4 w-4 shrink-0 opacity-50",
-    },
-    wrapper: {
-      default: "flex items-center border-b px-3",
-    },
   },
   defaultVariants: {
     variant: "default",
-    search: "default",
-    wrapper: "default",
   },
 });
 
@@ -76,10 +68,10 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
 };
 
 const CommandInput = React.forwardRef<React.ElementRef<typeof CommandPrimitive.Input>, InputProps>(
-  ({ className, variant, search, wrapper, ...props }, ref) => (
+  ({ className, variant, ...props }, ref) => (
     // eslint-disable-next-line react/no-unknown-property
-    <div className={cn(inputVariants({ wrapper }))} cmdk-input-wrapper="">
-      <Search className={cn(inputVariants({ search }))} />
+    <div className="flex items-center border-b px-3" cmdk-input-wrapper="">
+      <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
       <CommandPrimitive.Input
         ref={ref}
         className={cn(inputVariants({ variant, className }))}
