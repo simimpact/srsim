8b9d617cf30ab0e16740bb405a1663435d2ed5c3
Connor Kounthapanya <ckountha@gmail.com>
2023-06-18 05:00:49 -0600

    Sushang initial setup

---
+++ internal/character/sushang/attack.go
@@ -0,0 +1,10 @@
+package sushang
+
+import (
+	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/key"
+)
+
+func (c *char) Attack(target key.TargetID, state info.ActionState) {
+
+}
