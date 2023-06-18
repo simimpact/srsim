6491f1e748048c68b5e02beffbccd29a7fd1e9c9
Connor Kounthapanya <53917585+connor-kounthapanya@users.noreply.github.com>
2023-06-17 20:56:05 -0600

    Implement Subscribe for More (#58)
    * Implement Cruising in the Stellar Sea
    
    * Refactor Cruising in the Stellar Sea
    
    * Implement Arrows
    
    * Update Adversarial behavior flags and Arrows status type
    
    * Implement Only Silence Remains
    
    * Implement Adversarial
    
    * Fix Only Silence Remains
    
    * Implement River Flows in Spring
    
    * Update River Flows in Spring buff status type
    
    * Implement Subscribe for More

--- pkg/key/lightcone.go
+++ pkg/key/lightcone.go
@@ -18,6 +18,7 @@ const (
 	Adversarial             LightCone = "adversarial"
 	OnlySilenceRemains      LightCone = "only_silence_remains"
 	RiverFlowsinSpring      LightCone = "river_flows_in_spring"
+	SubscribeforMore        LightCone = "subscribe_for_more"
 )
 
 // Nihility
