b4a499251fad7c88c960658484f7027bf986b9f6
Keshav <keshavshyper6325@gmail.com>
2023-06-19 16:05:19 +0530

    Implemented Passerby of Wandering Cloud

--- pkg/key/relic.go
+++ pkg/key/relic.go
@@ -4,8 +4,9 @@ type Relic string
 
 // Cavern
 const (
-	MusketeerOfWildWheat  Relic = "musketeer_of_wild_wheat"
-	HunterOfGlacialForest Relic = "hunter_of_glacial_forest"
+	MusketeerOfWildWheat     Relic = "musketeer_of_wild_wheat"
+	HunterOfGlacialForest    Relic = "hunter_of_glacial_forest"
+	PasserbyOfWanderingCloud Relic = "passerby-of-wandering-cloud"
 )
 
 // Planar
