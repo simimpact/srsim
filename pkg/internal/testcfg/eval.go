c2d0a387fbf8d7839d0a31463cef26b829e5453f
imring <vitaliyvorobets25@gmail.com>
2023-06-21 13:11:30 +0300

    fix eval.go for log testing

--- pkg/internal/testcfg/eval.go
+++ pkg/internal/testcfg/eval.go
@@ -2,27 +2,15 @@ package testcfg
 
 import (
 	"context"
+
 	"github.com/simimpact/srsim/pkg/gcs/eval"
 	"github.com/simimpact/srsim/pkg/gcs/parse"
 )
 
 const actions = `
-register_skill_cb(0, fn () { return true; });
-
-let value = true;
-register_skill_cb(1, fn () {
-	value = !value;
-	return value;
-});
-
-let use = false;
-register_burst_cb(0, fn () {
-	if use {
-		use = false;
-		return true;
-	}
-	return false;
-});
+set_default_action(danheng, attack(LowestHP));
+register_skill_cb(danheng, fn () { return skill(LowestHP); });
+register_ult_cb(danheng, fn () { return ult(LowestHP); });
 `
 
 func StandardTestEval() *eval.Eval {
@@ -31,13 +19,5 @@ func StandardTestEval() *eval.Eval {
 	if err != nil {
 		panic(err)
 	}
-
-	e := &eval.Eval{AST: res.Program}
-	ctx := context.Background()
-	ok := e.Init(ctx)
-	if !ok {
-		err := <-e.Err
-		panic(err)
-	}
-	return e
+	return eval.New(res.Program, context.Background())
 }
