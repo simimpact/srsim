ece2e091b2b378248cd743d85258cdf99d0beecc
Othi <mnpq.raven@gmail.com>
2023-06-27 19:42:44 +0700

    [LC] Implement This Is Me

--- internal/lightcone/preservation/thisisme/thisisme.go
+++ internal/lightcone/preservation/thisisme/thisisme.go
@@ -12,8 +12,13 @@ import (
 )
 
 const (
-	mod key.Modifier = "this-is-me"
+	mod    key.Modifier = "this-is-me"
+	modUlt key.Modifier = "this-is-me-ult"
 )
+
+type State struct {
+	ultBonus float64
+}
 
 // Increases the wearer's DEF by 16%/20%/24%/28%/32%. Increases the DMG of the
 // wearer when they use their Ultimate by 60%/75%/90%/105%/120% of the wearer's
@@ -27,25 +32,36 @@ func init() {
 		Promotions:    promotions,
 	})
 
-	modifier.Register(mod, modifier.Config{
+	modifier.Register(modUlt, modifier.Config{
 		Listeners: modifier.Listeners{
-			OnBeforeHit: func(mod *modifier.ModifierInstance, e event.HitStartEvent) {},
-			OnAfterHit:  func(mod *modifier.ModifierInstance, e event.HitEndEvent) {},
+			OnBeforeHit: onBeforeHit,
+			OnAfterHit:  onAfterHit,
 		},
 	})
 }
 
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
 	amt := 0.12 + 0.04*float64(lc.Imposition)
+	ultDmg := 0.45 + 0.15*float64(lc.Imposition)
 
 	engine.AddModifier(owner, info.Modifier{
 		Name:   mod,
 		Source: owner,
 		Stats:  info.PropMap{prop.DEFPercent: amt},
+		State:  State{ultDmg},
 	})
 }
 
+// increase ult damage
 func onBeforeHit(mod *modifier.ModifierInstance, e event.HitStartEvent) {
+	state := mod.State().(State)
+	if e.Hit.AttackType == model.AttackType_ULT {
+		mod.Engine().AddModifier(mod.Owner(), info.Modifier{
+			Name:   modUlt,
+			Source: mod.Owner(),
+			Stats:  info.PropMap{prop.DEFPercent: state.ultBonus},
+		})
+	}
 }
 
 // remove modifier so next ult deals ult dmg + only 1x bonus from this lc
