c64523bffaea9a3f280483fa41598863262ca4e2
Kisa <janbeier0@gmail.com>
2023-06-28 17:34:04 +0200

    implemented skil.go

--- internal/character/gepard/skill.go
+++ internal/character/gepard/skill.go
@@ -1,10 +1,32 @@
 package gepard
 
 import (
+	"github.com/simimpact/srsim/internal/global/common"
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
 )
 
 func (c *char) Skill(target key.TargetID, state info.ActionState) {
-	
+	c.engine.Attack(info.Attack{
+		Source:     c.id,
+		Targets:    []key.TargetID{target},
+		DamageType: model.DamageType_ICE,
+		AttackType: model.AttackType_NORMAL,
+		BaseDamage: info.DamageMap{
+			model.DamageFormula_BY_ATK: skillDMG[c.info.SkillLevelIndex()],
+		},
+		StanceDamage: 60.0,
+		EnergyGain:   30.0,
+	})
+
+	c.engine.AddModifier(target, info.Modifier{
+		Name:   common.Freeze,
+		Source: c.id,
+		State: common.FreezeState{
+			DamagePercentage: skillFreezeDMG[c.info.SkillLevelIndex()],
+		},
+		Chance:   0.65,
+		Duration: 1,
+	})
 }
