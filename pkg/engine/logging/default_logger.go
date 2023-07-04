7ea0aa34f4aeaa2d42cd393b860c3521af555364
Kyle Bartz <mail@kyle.fail>
2023-07-04 06:31:27 +0000

    Replaced logger Singleton with an array

--- pkg/engine/logging/default_logger.go
+++ pkg/engine/logging/default_logger.go
@@ -2,17 +2,11 @@ package logging
 
 import (
 	"encoding/json"
-	"fmt"
 	"strings"
 )
 
 type DefaultLogger struct {
 	entries []string
-}
-
-type logEventWrapper struct {
-	EventName string      `json:"name"`
-	Data      interface{} `json:"event"`
 }
 
 func NewDefaultLogger() *DefaultLogger {
@@ -21,10 +15,7 @@ func NewDefaultLogger() *DefaultLogger {
 
 // Log registers an event to the DefaultLogger entry
 func (l *DefaultLogger) Log(e interface{}) {
-	wrappedEv := &logEventWrapper{
-		EventName: strings.TrimPrefix(fmt.Sprintf("%T", e), "event."),
-		Data:      e,
-	}
+	wrappedEv := Wrap(e)
 	res, err := json.Marshal(wrappedEv)
 	if err != nil {
 		return
