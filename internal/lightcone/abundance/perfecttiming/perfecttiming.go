1cab2802b59fa8887231110b7faa465d2ae2f249
Ryan Older <ryanolder67@gmail.com>
2023-07-05 18:54:23 +0700

    added BattleStart & ActionEnd subscribe + func sig

--- internal/lightcone/abundance/perfecttiming/perfecttiming.go
+++ internal/lightcone/abundance/perfecttiming/perfecttiming.go
@@ -3,6 +3,7 @@ package perfecttiming
 import (
 	"github.com/simimpact/srsim/pkg/engine"
 	"github.com/simimpact/srsim/pkg/engine/equip/lightcone"
+	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/engine/modifier"
 	"github.com/simimpact/srsim/pkg/engine/prop"
@@ -27,30 +28,46 @@ func init() {
 	})
 	modifier.Register(PTEffRes, modifier.Config{})
 
-	modifier.Register(PTHealBoost, modifier.Config{})
+	modifier.Register(PTHealBoost, modifier.Config{
+		Stacking: modifier.ReplaceBySource,
+	})
 }
 
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
 	effResAmt := 0.12 + 0.04*float64(lc.Imposition)
+	healBuffAmt := float64(0)
 	engine.AddModifier(owner, info.Modifier{
 		Name:   PTEffRes,
 		Source: owner,
 		Stats:  info.PropMap{prop.EffectRES: effResAmt},
 	})
+	// OnBattleStart -> giveHealBoost to owner.
+	engine.Events().BattleStart.Subscribe(func(e event.BattleStart) {
+		for _, char := range e.CharStats {
+			if char.ID() == owner {
+				giveHealBuff(engine, char, lc.Imposition, healBuffAmt)
+			}
+		}
+	})
+	// OnActionEnd -> recalc HealBoost.
+	engine.Events().ActionEnd.Subscribe(func(e event.ActionEnd) {
+		healBuffAmt = giveHealBuff(engine, engine.Stats(owner), lc.Imposition, healBuffAmt)
+	})
 }
 
-func giveHealBuff(mod *modifier.Instance) {
+func giveHealBuff(engine engine.Engine, owner *info.Stats, imposition int, prevBuff float64) float64 {
 	// take user's eff res value post-gear buff.
-	currEffRes := mod.Engine().Stats(mod.Owner()).EffectRES()
+	currEffRes := engine.Stats(owner.ID()).EffectRES()
 	// out. heal buff depend on eff res post-buff. can't take in base stats.
-	healBuffAmt := currEffRes * (0.30 + 0.03*mod.State().(float64))
-	maxHealBuffAmt := 0.12 + 0.03*mod.State().(float64)
+	healBuffAmt := currEffRes*0.30 + 0.03*float64(imposition)
+	maxHealBuffAmt := 0.12 + 0.03*float64(imposition)
 	if healBuffAmt > maxHealBuffAmt {
 		healBuffAmt = maxHealBuffAmt
 	}
-	mod.Engine().AddModifier(mod.Owner(), info.Modifier{
+	engine.AddModifier(owner.ID(), info.Modifier{
 		Name:   PTHealBoost,
-		Source: mod.Owner(),
+		Source: owner.ID(),
 		Stats:  info.PropMap{prop.HealBoost: healBuffAmt},
 	})
+	return healBuffAmt
 }
