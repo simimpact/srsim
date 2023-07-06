d4c3287d15ddfa9769f63d5408f25986407a9b92
Othi <mnpq.raven@gmail.com>
2023-07-06 22:46:43 +0700

    delete unnecessary context menu

--- ui/packages/web/src/routes/Root/page.tsx
+++ ui/packages/web/src/routes/Root/page.tsx
@@ -1,18 +1,3 @@
-import {
-  ContextMenu,
-  ContextMenuCheckboxItem,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuLabel,
-  ContextMenuRadioGroup,
-  ContextMenuRadioItem,
-  ContextMenuSeparator,
-  ContextMenuShortcut,
-  ContextMenuSub,
-  ContextMenuSubContent,
-  ContextMenuSubTrigger,
-  ContextMenuTrigger,
-} from "@/components/Primitives/ContextMenu";
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/Primitives/HoverCard";
 import { LogViewer } from "@/components/Sim/Logging/LogViewer";
 import { CharacterLineup } from "./CharacterLineup";
@@ -40,51 +25,9 @@ const Root = () => {
             </HoverCardContent>
           </HoverCard>
         </div>
-        <ContextMenu>
-          <ContextMenuTrigger className="bg-accent text-accent-foreground flex h-full mx-8 rounded-md p-10">
-            <LogViewer placeholder="test" />
-          </ContextMenuTrigger>
-          <ContextMenuContent>
-            <ContextMenuItem inset>
-              Back
-              <ContextMenuShortcut>⌘[</ContextMenuShortcut>
-            </ContextMenuItem>
-            <ContextMenuItem inset disabled>
-              Forward
-              <ContextMenuShortcut>⌘]</ContextMenuShortcut>
-            </ContextMenuItem>
-            <ContextMenuItem inset>
-              Reload
-              <ContextMenuShortcut>⌘R</ContextMenuShortcut>
-            </ContextMenuItem>
-            <ContextMenuSub>
-              <ContextMenuSubTrigger inset>More Tools</ContextMenuSubTrigger>
-              <ContextMenuSubContent className="w-48">
-                <ContextMenuItem>
-                  Save Page As...
-                  <ContextMenuShortcut>⇧⌘S</ContextMenuShortcut>
-                </ContextMenuItem>
-                <ContextMenuItem>Create Shortcut...</ContextMenuItem>
-                <ContextMenuItem>Name Window...</ContextMenuItem>
-                <ContextMenuSeparator />
-                <ContextMenuItem>Developer Tools</ContextMenuItem>
-              </ContextMenuSubContent>
-            </ContextMenuSub>
-            <ContextMenuSeparator />
-            <ContextMenuCheckboxItem checked>
-              Show Bookmarks Bar
-              <ContextMenuShortcut>⌘⇧B</ContextMenuShortcut>
-            </ContextMenuCheckboxItem>
-            <ContextMenuCheckboxItem>Show Full URLs</ContextMenuCheckboxItem>
-            <ContextMenuSeparator />
-            <ContextMenuRadioGroup value="pedro">
-              <ContextMenuLabel inset>People</ContextMenuLabel>
-              <ContextMenuSeparator />
-              <ContextMenuRadioItem value="pedro">Pedro Duarte</ContextMenuRadioItem>
-              <ContextMenuRadioItem value="colm">Colm Tuite</ContextMenuRadioItem>
-            </ContextMenuRadioGroup>
-          </ContextMenuContent>
-        </ContextMenu>
+        <div className="bg-accent text-accent-foreground flex flex-col h-full mx-8 rounded-md p-10">
+          <LogViewer placeholder="test" />
+        </div>
       </div>
     </div>
   );
