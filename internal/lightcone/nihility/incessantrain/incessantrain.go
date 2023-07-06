0d504d53ead84032b15418e63fec724a08725c98
Ryan Older <ryanolder67@gmail.com>
2023-07-06 20:42:38 +0700

    quick DM analysis

--- internal/lightcone/nihility/incessantrain/incessantrain.go
+++ internal/lightcone/nihility/incessantrain/incessantrain.go
@@ -13,6 +13,12 @@ import (
 // increases the wearer's CRIT Rate by 12%. After the wearer uses their Basic ATK, Skill, or Ultimate,
 // there is a 100% base chance to implant Aether Code on a random hit target that does not yet have it.
 // Targets with Aether Code receive 12% increased DMG for 1 turn.
+// Apparent mods : EHR, CRBoost, Aether Code implant (dmgTakenIncrease)
+
+// DM Listeners : OnBeforeHitAll = add CR, onAfterAttack = Retarget, 1 turn cooldown : AetherCode,
+// OnBeforeSkillUse = set turn cooldown to 1, OnAfterSkillUse = apply AC if not yet have it.
+// => set turn cd to 0.
+// DmgTakenUp = OnStack, modifier, AllDmgTypeTakenRatio, stacking = replace, OnStart = AddModifier Main
 
 func init() {
 	lightcone.Register(key.IncessantRain, lightcone.Config{
