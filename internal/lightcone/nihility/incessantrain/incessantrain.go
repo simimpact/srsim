484d13962fcf039af60dfcb0900a83f7c5e5334f
Ryan Older <ryanolder67@gmail.com>
2023-07-07 14:50:55 +0700

    init rework + first aether code implementation

--- internal/lightcone/nihility/incessantrain/incessantrain.go
+++ internal/lightcone/nihility/incessantrain/incessantrain.go
@@ -12,9 +12,10 @@ import (
 )
 
 const (
-	ehrNCritCheck key.Modifier = "incessant-rain-ehr-crit-check"
-	critMod       key.Modifier = "incessant-rain-crit-rate"
-	aetherCode    key.Modifier = "incessant-rain-aether-code"
+	ehrNCritCheck    key.Modifier = "incessant-rain-ehr-crit-check"
+	critMod          key.Modifier = "incessant-rain-crit-rate"
+	aetherCodeApply  key.Modifier = "incessant-rain-aether-code-applier"
+	aetherCodeDebuff key.Modifier = "incessant-rain-aether-code-debuff"
 )
 
 // Desc : Increases the wearer's Effect Hit Rate by 24%.
@@ -22,16 +23,6 @@ const (
 // increases the wearer's CRIT Rate by 12%. After the wearer uses their Basic ATK, Skill, or Ultimate,
 // there is a 100% base chance to implant Aether Code on a random hit target that does not yet have it.
 // Targets with Aether Code receive 12% increased DMG for 1 turn.
-// Apparent mods : EHR, CRBoost, Aether Code implant (dmgTakenIncrease)
-
-// Quick Imp Plan :
-// EHR perm buff : add it on create.
-// Modifiers :
-// - Crit Rate : Listener = OnBeforeHitAll, check each enemy if it has 3 debuffs => add critrate boost
-// - aetherCode : Listener = OnAfterAction, choose 1 among hit targets on last atk, aetherCode 100% BC
-//	 -> check first if target already have AC
-//	=> OnBeforeAction = set cd to 1
-//  => set aetherCode as enemy modifier DmgTakenUp.
 
 func init() {
 	lightcone.Register(key.IncessantRain, lightcone.Config{
@@ -45,18 +36,21 @@ func init() {
 			OnBeforeHitAll: critRateBoost,
 		},
 	})
-	modifier.Register(aetherCode, modifier.Config{
+	modifier.Register(critMod, modifier.Config{
+		StatusType: model.StatusType_STATUS_BUFF, // is this critrate boost a "real buff"?
+	})
+	modifier.Register(aetherCodeApply, modifier.Config{
 		Listeners: modifier.Listeners{
-			OnBeforeAction: resetCooldown,
-			OnAfterAction:  applyDebuffOnce,
+			OnAfterAttack: applyDebuffOnce,
 		},
 	})
-	modifier.Register(critMod, modifier.Config{
-		StatusType: model.StatusType_STATUS_BUFF, // is this critrate boost a mod?
+	modifier.Register(aetherCodeDebuff, modifier.Config{
+		StatusType: model.StatusType_STATUS_DEBUFF,
 	})
 }
 
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
+	// ehr constant + crit with condition
 	ehrAmt := 0.20 + 0.04*float64(lc.Imposition)
 	critAmt := 0.10 + 0.02*float64(lc.Imposition)
 	engine.AddModifier(owner, info.Modifier{
@@ -64,6 +58,13 @@ func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
 		Source: owner,
 		Stats:  info.PropMap{prop.EffectHitRate: ehrAmt},
 		State:  critAmt,
+	})
+	// Aether Code applier
+	dmgTakenAmt := 0.10 + 0.02*float64(lc.Imposition)
+	engine.AddModifier(owner, info.Modifier{
+		Name:   aetherCodeApply,
+		Source: owner,
+		State:  dmgTakenAmt,
 	})
 }
 
@@ -79,12 +80,29 @@ func critRateBoost(mod *modifier.Instance, e event.HitStart) {
 		})
 	}
 }
-// reset aethercode cooldown when char turn is up
-func resetCooldown(mod *modifier.Instance, e event.ActionStart) {
-
-}
 
 // retarget with 1 chosen(among non-AC-applied). apply dmgTakenUp with 100% basechance. cooldown tick.
-func applyDebuffOnce(mod *modifier.Instance, e event.ActionEnd) {
+func applyDebuffOnce(mod *modifier.Instance, e event.AttackEnd) {
+	// fetch enemy list hit by this attack
+	enemyList := e.Targets
+	dmgTakenAmt := mod.State().(float64)
+	var validEnemyList []key.TargetID
 
+	// validEnemyList should only contain non-dead, non-implanted enemies
+	for _, enemy := range enemyList {
+		// is enemy alive and does enemy not have aether code yet. if so, append. (how to do alive check?)
+		if mod.Engine().IsValid(enemy) && !mod.Engine().HasModifier(enemy, aetherCodeDebuff) {
+			validEnemyList = append(validEnemyList, enemy)
+		}
+	}
+	if validEnemyList != nil {
+		// choose one enemy, apply debuff to them.
+		chosenOne := validEnemyList[mod.Engine().Rand().Intn(len(validEnemyList))]
+		mod.Engine().AddModifier(chosenOne, info.Modifier{
+			Name:   aetherCodeDebuff,
+			Source: mod.Owner(),
+			Stats:  info.PropMap{prop.AllDamageTaken: dmgTakenAmt},
+			Chance: 1.0,
+		})
+	}
 }
