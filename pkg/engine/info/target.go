d44ba25e9f786bad7d2a40f3d7ab673b0f89ca6e
Ryan Older <ryanolder67@gmail.com>
2023-07-08 06:44:00 +0700

    added Retarget struct

--- pkg/engine/info/target.go
+++ pkg/engine/info/target.go
@@ -1,6 +1,22 @@
 package info
 
+import "github.com/simimpact/srsim/pkg/key"
+
 type TargetClass int
+
+type Retarget struct {
+	// List of all targets to be filtered from
+	Targets []key.TargetID
+
+	// Filter function
+	Filter func(targets []key.TargetID) []key.TargetID
+
+	// Maximum amount of targets to select from the filtered target list
+	Max int
+
+	// Option to include targets in limbo(0 HP)
+	IncludeLimbo bool
+}
 
 const (
 	ClassInvalid TargetClass = iota
