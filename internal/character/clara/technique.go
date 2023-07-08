1756f4b02c572afada6398e3fdee850a4133daf3
Othi <mnpq.raven@gmail.com>
2023-07-08 14:00:52 +0700

    technique

--- internal/character/clara/technique.go
+++ internal/character/clara/technique.go
@@ -2,9 +2,19 @@ package clara
 
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
 )
 
+const (
+	Technique key.Modifier = "clara-technique-aggro"
+)
+
 func (c *char) Technique(target key.TargetID, state info.ActionState) {
-	
+	c.engine.AddModifier(c.id, info.Modifier{
+		Name:     Technique,
+		Source:   c.id,
+		Stats:    info.PropMap{prop.AggroPercent: 5},
+		Duration: 2,
+	})
 }
