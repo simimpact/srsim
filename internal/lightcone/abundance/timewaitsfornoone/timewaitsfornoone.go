acbbc6f9293ddded88e4d86df84745b31decf158
Ryan Older <ryanolder67@gmail.com>
2023-07-10 17:32:09 +0700

    apparent modifiers from desc

--- internal/lightcone/abundance/timewaitsfornoone/timewaitsfornoone.go
+++ internal/lightcone/abundance/timewaitsfornoone/timewaitsfornoone.go
@@ -8,6 +8,19 @@ import (
 	"github.com/simimpact/srsim/pkg/model"
 )
 
+// Desc : Increases the wearer's Max HP by 18% and Outgoing Healing by 12%.
+// When the wearer heals allies, record the amount of Outgoing Healing.
+// When any ally launches an attack, a random attacked enemy takes Additional DMG
+// equal to 36% of the recorded Outgoing Healing value.
+// The type of this Additional DMG is of the same Type as the wearer's.
+// This Additional DMG is not affected by other buffs, and can only occur 1 time per turn.
+
+// Apparent modifiers :
+// HP buff and outgoing healing -> on Create. add static
+// OnAfterHeal -> record heal amt (pass it to a struct w/ pointer?)
+// OnAfterAttack -> pick random 1 enemy, apply x% dmg based on healAmt + element same as holder
+// 1 time per turn -> onBeforeTurn : refresh, pass 1 turn to struct or inherent duration?
+
 func init() {
 	lightcone.Register(key.TimeWaitsforNoOne, lightcone.Config{
 		CreatePassive: Create,
