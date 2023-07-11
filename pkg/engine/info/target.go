5efe629694ec5b39eb3ff63e7f249e633e99d962
Ryan Older <ryanolder67@gmail.com>
2023-07-11 10:31:15 +0700

    changed Retarget struct docs

--- pkg/engine/info/target.go
+++ pkg/engine/info/target.go
@@ -8,10 +8,11 @@ type Retarget struct {
 	// List of all targets to be filtered from.
 	Targets []key.TargetID
 
-	// Filter function (optional). Used to filter Targets based on some condition(s).
-	Filter func(target key.TargetID) bool `exhaustruct:"optional"`
+	// Filter function. Used to filter Targets based on some condition(s).
+	Filter func(target key.TargetID) bool
 
-	// Maximum amount of targets to select from the filtered target list. defaults to 1.
+	// Maximum amount of targets to select from the filtered target list.
+	// Defaults to the size of filtered list
 	Max int `exhaustruct:"optional"`
 
 	// Option to include targets in limbo(0 HP).
