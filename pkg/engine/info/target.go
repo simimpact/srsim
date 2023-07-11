ff83fa1e0031ecdfd21d443c3508d1156e15002f
Ryan Older <ryanolder67@gmail.com>
2023-07-11 09:42:15 +0700

    modify Retarget struct

--- pkg/engine/info/target.go
+++ pkg/engine/info/target.go
@@ -5,17 +5,20 @@ import "github.com/simimpact/srsim/pkg/key"
 type TargetClass string
 
 type Retarget struct {
-	// List of all targets to be filtered from
+	// List of all targets to be filtered from.
 	Targets []key.TargetID
 
-	// Filter function
-	Filter func(targets []key.TargetID) []key.TargetID
+	// Filter function (optional). Used to filter Targets based on some condition(s).
+	Filter func(target key.TargetID) bool `exhaustruct:"optional"`
 
-	// Maximum amount of targets to select from the filtered target list
-	Max int
+	// Maximum amount of targets to select from the filtered target list. defaults to 1.
+	Max int `exhaustruct:"optional"`
 
-	// Option to include targets in limbo(0 HP)
-	IncludeLimbo bool
+	// Option to include targets in limbo(0 HP).
+	IncludeLimbo bool `exhaustruct:"optional"`
+
+	// Option to disable default random retarget behavior.
+	DisableRandom bool `exhaustruct:"optional"`
 }
 
 const (
