5dc5a154950d56ee8ddd6267c2dc049c77ea63ed
iCarus <ardormaiden88@gmail.com>
2023-07-15 14:05:33 +0800

    Testframe: added Char asserters

--- tests/teststub/characters.go
+++ tests/teststub/characters.go
@@ -1,6 +1,8 @@
 package teststub
 
 import (
+	"testing"
+
 	"github.com/simimpact/srsim/pkg/engine/attribute"
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/key"
@@ -12,6 +14,7 @@ import (
 // -- before sim starts: adding new chars to the sim, modifying existing ones
 // -- during sim: checking the state of existing chars
 type Characters struct {
+	t               *testing.T
 	cfg             *model.SimConfig
 	testChars       []*Character
 	attributes      attribute.Manager
@@ -20,6 +23,7 @@ type Characters struct {
 
 // Character is a test instance of a character which provides various Checker methods and retrievers
 type Character struct {
+	t     *testing.T
 	model *model.Character
 	key   key.TargetID
 	stat  func(id key.TargetID) *info.Stats
@@ -49,16 +53,6 @@ func (s *Characters) AddCharacterEval(eval testeval.ActionEval, idx int) {
 	s.customFunctions[idx] = eval
 }
 
-// GetCharacterTargetID fetches the key.TargetID value for the character at idx that the Sim is using.
-// This is used for most state checks
-func (s *Characters) GetCharacterTargetID(idx int) key.TargetID {
-	if idx >= len(s.testChars) {
-		LogError("invalid idx %d, insufficient characters", idx)
-		panic("Invalid index")
-	}
-	return s.testChars[idx].key
-}
-
 // Character fetches the test instance of the first character with the given key. This is useful for adding clarity
 // or todo: cases where the character is added post-combat start
 func (s *Characters) Character(key key.Character) *Character {
@@ -69,14 +63,6 @@ func (s *Characters) Character(key key.Character) *Character {
 	}
 	LogError("Character Key %s is not in the SimConfig", key.String())
 	return nil
-}
-
-// GetCharacterInfo fetches the info.Stats value for the character at idx. Useful for verifying energy state etc.
-func (s *Characters) GetCharacterInfo(idx int) *info.Stats {
-	if idx >= len(s.testChars) {
-		LogError("invalid idx %d, insufficient characters", idx)
-	}
-	return s.testChars[idx].Stats()
 }
 
 func (s *Characters) getCharacterEval(idx int) testeval.ActionEval {
@@ -95,5 +81,6 @@ func (s *Characters) init(characters []key.TargetID) {
 	for i := range s.testChars {
 		s.testChars[i].key = characters[i]
 		s.testChars[i].stat = s.attributes.Stats
+		s.testChars[i].t = s.t
 	}
 }
