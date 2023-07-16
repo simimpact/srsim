c8c9a0f8649b1a0c15daf0edd92e22aef2e574fc
Kyle Bartz <mail@kyle.fail>
2023-07-16 06:24:13 +0000

    Moved Retarget and AdjacentTo a helper pkg

--- pkg/engine/target/retarget_test.go
+++ pkg/engine/target/retarget_test.go
@@ -1,4 +1,4 @@
-package simulation_test
+package target_test
 
 import (
 	"math/rand"
@@ -6,28 +6,22 @@ import (
 
 	"github.com/golang/mock/gomock"
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/target"
 	"github.com/simimpact/srsim/pkg/key"
-	"github.com/simimpact/srsim/pkg/simulation"
 	"github.com/simimpact/srsim/tests/mock"
 	"github.com/stretchr/testify/assert"
 )
 
-// helper to build sim with necessary mocks
-func NewSim(t *testing.T, seed int64) (*simulation.Simulation, *mock.MockAttribute) {
+func NewRandAndAttr(t *testing.T, seed int64) (*rand.Rand, *mock.MockAttribute) {
 	mockCtrl := gomock.NewController(t)
-	attr := mock.NewMockAttribute(mockCtrl)
-	sim := &simulation.Simulation{
-		Attr:   attr,
-		Random: rand.New(rand.NewSource(seed)),
-	}
-	return sim, attr
+	return rand.New(rand.NewSource(seed)), mock.NewMockAttribute(mockCtrl)
 }
 
 // test var : Targets list
 func TestRetargetEmptyTargets(t *testing.T) {
-	sim, _ := NewSim(t, 1)
+	rand, attr := NewRandAndAttr(t, 1)
 
-	result := sim.Retarget(info.Retarget{
+	result := target.Retarget(rand, attr, info.Retarget{
 		Targets: []key.TargetID{},
 		Filter:  func(target key.TargetID) bool { return target == 2 },
 		Max:     3,
@@ -38,12 +32,12 @@ func TestRetargetEmptyTargets(t *testing.T) {
 
 // test var : Filter func
 func TestRetargetNoFilterFunc(t *testing.T) {
-	sim, attr := NewSim(t, 100)
+	rand, attr := NewRandAndAttr(t, 100)
 
 	// for all targets, return full health in this test
 	attr.EXPECT().HPRatio(gomock.Any()).Return(1.0).AnyTimes()
 
-	result := sim.Retarget(info.Retarget{
+	result := target.Retarget(rand, attr, info.Retarget{
 		Targets: []key.TargetID{1, 2, 3, 4},
 	})
 
@@ -52,12 +46,12 @@ func TestRetargetNoFilterFunc(t *testing.T) {
 }
 
 func TestRetargetEmptyFiltered(t *testing.T) {
-	sim, attr := NewSim(t, 1)
+	rand, attr := NewRandAndAttr(t, 1)
 
 	// for all targets, return full health in this test
 	attr.EXPECT().HPRatio(gomock.Any()).Return(1.0).AnyTimes()
 
-	result := sim.Retarget(info.Retarget{
+	result := target.Retarget(rand, attr, info.Retarget{
 		Targets: []key.TargetID{1, 2, 3, 4},
 		Filter:  func(target key.TargetID) bool { return false },
 		Max:     3,
@@ -69,12 +63,12 @@ func TestRetargetEmptyFiltered(t *testing.T) {
 
 // test variable : Max
 func TestRetargetMaxZero(t *testing.T) {
-	sim, attr := NewSim(t, 1)
+	rand, attr := NewRandAndAttr(t, 1)
 
 	// for all targets, return full health in this test
 	attr.EXPECT().HPRatio(gomock.Any()).Return(1.0).AnyTimes()
 
-	result := sim.Retarget(info.Retarget{
+	result := target.Retarget(rand, attr, info.Retarget{
 		Targets: []key.TargetID{1, 2, 3},
 		Filter:  func(target key.TargetID) bool { return target == 2 },
 	})
@@ -83,12 +77,12 @@ func TestRetargetMaxZero(t *testing.T) {
 }
 
 func TestRetargetMaxHigh(t *testing.T) {
-	sim, attr := NewSim(t, 1)
+	rand, attr := NewRandAndAttr(t, 1)
 
 	// for all targets, return full health in this test
 	attr.EXPECT().HPRatio(gomock.Any()).Return(1.0).AnyTimes()
 
-	result := sim.Retarget(info.Retarget{
+	result := target.Retarget(rand, attr, info.Retarget{
 		Targets: []key.TargetID{1, 2, 3},
 		Filter:  func(target key.TargetID) bool { return true },
 		Max:     100,
@@ -98,12 +92,12 @@ func TestRetargetMaxHigh(t *testing.T) {
 }
 
 func TestRetargetMaxLow(t *testing.T) {
-	sim, attr := NewSim(t, 123)
+	rand, attr := NewRandAndAttr(t, 123)
 
 	// for all targets, return full health in this test
 	attr.EXPECT().HPRatio(gomock.Any()).Return(1.0).AnyTimes()
 
-	result := sim.Retarget(info.Retarget{
+	result := target.Retarget(rand, attr, info.Retarget{
 		Targets: []key.TargetID{1, 2, 3, 4, 5},
 		Filter:  func(target key.TargetID) bool { return true },
 		Max:     3,
@@ -114,12 +108,12 @@ func TestRetargetMaxLow(t *testing.T) {
 
 // test var : no random
 func TestRetargetNoRandom(t *testing.T) {
-	sim, attr := NewSim(t, 1)
+	rand, attr := NewRandAndAttr(t, 1)
 
 	// for all targets, return full health in this test
 	attr.EXPECT().HPRatio(gomock.Any()).Return(1.0).AnyTimes()
 
-	result := sim.Retarget(info.Retarget{
+	result := target.Retarget(rand, attr, info.Retarget{
 		Targets:       []key.TargetID{1, 2, 3, 4},
 		Filter:        func(target key.TargetID) bool { return true },
 		Max:           3,
@@ -131,7 +125,7 @@ func TestRetargetNoRandom(t *testing.T) {
 
 // test variable : Ignore Limbo
 func TestRetargetIgnoresLimbo(t *testing.T) {
-	sim, attr := NewSim(t, 124)
+	rand, attr := NewRandAndAttr(t, 124)
 	targets := []key.TargetID{1, 2, 3}
 
 	// target 1, 0 HP
@@ -141,7 +135,7 @@ func TestRetargetIgnoresLimbo(t *testing.T) {
 	attr.EXPECT().HPRatio(targets[1]).Return(0.5)
 	attr.EXPECT().HPRatio(targets[2]).Return(1.0)
 
-	result := sim.Retarget(info.Retarget{
+	result := target.Retarget(rand, attr, info.Retarget{
 		Targets: []key.TargetID{1, 2, 3},
 		Filter:  func(target key.TargetID) bool { return true },
 	})
@@ -151,7 +145,7 @@ func TestRetargetIgnoresLimbo(t *testing.T) {
 }
 
 func TestRetargetIncludeLimbo(t *testing.T) {
-	sim, attr := NewSim(t, 1)
+	rand, attr := NewRandAndAttr(t, 1)
 	targets := []key.TargetID{1, 2, 3}
 
 	// target 1, 0 HP
@@ -161,7 +155,7 @@ func TestRetargetIncludeLimbo(t *testing.T) {
 	attr.EXPECT().HPRatio(targets[1]).Return(0.5).AnyTimes()
 	attr.EXPECT().HPRatio(targets[2]).Return(1.0).AnyTimes()
 
-	result := sim.Retarget(info.Retarget{
+	result := target.Retarget(rand, attr, info.Retarget{
 		Targets:      []key.TargetID{1, 2, 3},
 		Filter:       func(target key.TargetID) bool { return true },
 		IncludeLimbo: true,
@@ -172,7 +166,7 @@ func TestRetargetIncludeLimbo(t *testing.T) {
 }
 
 func TestRetargetNoFilterNoLimbo(t *testing.T) {
-	sim, attr := NewSim(t, 125)
+	rand, attr := NewRandAndAttr(t, 125)
 	targets := []key.TargetID{1, 2, 3}
 
 	// target 1, 1.0 HP
@@ -182,7 +176,7 @@ func TestRetargetNoFilterNoLimbo(t *testing.T) {
 	attr.EXPECT().HPRatio(targets[1]).Return(0.0)
 	attr.EXPECT().HPRatio(targets[2]).Return(0.5)
 
-	result := sim.Retarget(info.Retarget{
+	result := target.Retarget(rand, attr, info.Retarget{
 		Targets: []key.TargetID{1, 2, 3},
 	})
 
