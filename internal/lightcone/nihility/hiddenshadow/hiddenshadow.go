02e0f89e0a8697fb6d26a8c131f8ce6cffbc8540
Ryan Older <ryanolder67@gmail.com>
2023-07-18 19:51:32 +0700

    dm extract

--- internal/lightcone/nihility/hiddenshadow/hiddenshadow.go
+++ internal/lightcone/nihility/hiddenshadow/hiddenshadow.go
@@ -8,6 +8,17 @@ import (
 	"github.com/simimpact/srsim/pkg/model"
 )
 
+// DESC : After using Skill, the wearer's next Basic ATK deals
+// Additional DMG equal to 60% of ATK to the target enemy.
+
+// DM :
+// OnAfterSkillUse : if skill, add _Sub mod
+// _Sub def : OnAfterAttack = if flag = 1, retarget Max 1, includeLimbo.
+// -> deal extra pursued dmg, param x% of lc holder's ATK.
+// OnBeforeSkillUse : set flag to 1.
+// OnAfterSkillUse : delete flag definition + modifier(wtf?)
+// OnStart : add _Main mod.
+
 func init() {
 	lightcone.Register(key.HiddenShadow, lightcone.Config{
 		CreatePassive: Create,
