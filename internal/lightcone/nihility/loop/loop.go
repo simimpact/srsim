21611bb9660065847726b041aa8c633edef0a1d4
Ryan Older <ryanolder67@gmail.com>
2023-07-18 22:19:01 +0700

    mod register

--- internal/lightcone/nihility/loop/loop.go
+++ internal/lightcone/nihility/loop/loop.go
@@ -3,7 +3,9 @@ package loop
 import (
 	"github.com/simimpact/srsim/pkg/engine"
 	"github.com/simimpact/srsim/pkg/engine/equip/lightcone"
+	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/modifier"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
 )
@@ -24,8 +26,17 @@ func init() {
 		Path:          model.Path_NIHILITY,
 		Promotions:    promotions,
 	})
+	modifier.Register(loop, modifier.Config{
+		Listeners: modifier.Listeners{
+			OnBeforeHitAll: boostDmgOnSlowed,
+		},
+	})
 }
 
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
+
+}
+
+func boostDmgOnSlowed(mod *modifier.Instance, e event.HitStart) {
 
 }
