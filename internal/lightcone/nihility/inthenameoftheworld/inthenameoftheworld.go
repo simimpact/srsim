ba33d32dd69b21fe543de819f698a80350777249
Ryan Older <ryanolder67@gmail.com>
2023-07-20 07:30:12 +0700

    init setup

---
+++ internal/lightcone/nihility/inthenameoftheworld/inthenameoftheworld.go
@@ -0,0 +1,22 @@
+package inthenameoftheworld
+
+import (
+	"github.com/simimpact/srsim/pkg/engine"
+	"github.com/simimpact/srsim/pkg/engine/equip/lightcone"
+	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
+)
+
+func init() {
+	lightcone.Register(key.IntheNameoftheWorld, lightcone.Config{
+		CreatePassive: Create,
+		Rarity:        5,
+		Path:          model.Path_NIHILITY,
+		Promotions:    promotions,
+	})
+}
+
+func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
+
+}
