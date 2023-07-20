497e6beddfd9865821b91b838caaad23d5ea939a
Ryan Older <ryanolder67@gmail.com>
2023-07-20 16:11:38 +0700

    remove DM notes

--- internal/lightcone/abundance/echoesofthecoffin/echoesofthecoffin.go
+++ internal/lightcone/abundance/echoesofthecoffin/echoesofthecoffin.go
@@ -26,15 +26,6 @@ type state struct {
 // Each attack can regenerate Energy up to 3 time(s) this way.
 // After the wearer uses their Ultimate, all allies gain 12 SPD for 1 turn.
 
-// DM :
-// OnAfterAttack : retarget on atktargetlist max 3. set enemyHit. modifySPNew. enemyHit to 0
-// OnAfterSkillUse : if Ult, addMod spdBuff,
-
-// impl :
-// OnAfterAttack : forEach hit enemies, add energy to holder. max 3
-// OnAfterAction : if action == ult, buff allies spd.
-// concl : 2 mods, 1 checker 1 ally spd buff.
-
 func init() {
 	lightcone.Register(key.EchoesoftheCoffin, lightcone.Config{
 		CreatePassive: Create,
