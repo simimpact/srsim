737870be22ef554a932d65530565265601f064cf
Ryan Older <ryanolder67@gmail.com>
2023-07-20 14:46:07 +0700

    DM glance + impl conclusion

--- internal/lightcone/abundance/echoesofthecoffin/echoesofthecoffin.go
+++ internal/lightcone/abundance/echoesofthecoffin/echoesofthecoffin.go
@@ -8,6 +8,20 @@ import (
 	"github.com/simimpact/srsim/pkg/model"
 )
 
+// Increases the wearer's ATK by 24%. After the wearer uses an attack,
+// for each different enemy target the wearer hits, regenerates 3 Energy.
+// Each attack can regenerate Energy up to 3 time(s) this way.
+// After the wearer uses their Ultimate, all allies gain 12 SPD for 1 turn.
+
+// DM :
+// OnAfterAttack : retarget on atktargetlist max 3. set enemyCount. modifySPNew. enemyCount to 0
+// OnAfterSkillUse : if Ult, addMod spdBuff,
+
+// impl :
+// OnAfterAttack : forEach hit enemies, add energy to holder. max 3
+// OnAfterAction : if action == ult, buff allies spd.
+// concl : 2 mods, 1 checker 1 ally spd buff.
+
 func init() {
 	lightcone.Register(key.EchoesoftheCoffin, lightcone.Config{
 		CreatePassive: Create,
