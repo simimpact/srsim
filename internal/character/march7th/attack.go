f6382cdc0a9502e7f0c0a4c52727328a4984cdca
aaron-hwang <aaronhwang22@gmail.com>
2023-07-22 18:50:05 -0400

    Added march7th basic atk

--- internal/character/march7th/attack.go
+++ internal/character/march7th/attack.go
@@ -3,8 +3,22 @@ package march7th
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
 )
 
-func (c *char) Attack(target key.TargetID, state info.ActionState) {
+const Normal key.Attack = "march7th-normal"
 
+func (c *char) Attack(target key.TargetID, state info.ActionState) {
+	c.engine.Attack(info.Attack{
+		Key:        Normal,
+		Targets:    []key.TargetID{target},
+		Source:     c.id,
+		AttackType: model.AttackType_NORMAL,
+		DamageType: model.DamageType_ICE,
+		BaseDamage: info.DamageMap{
+			model.DamageFormula_BY_ATK: atk[c.info.AttackLevelIndex()],
+		},
+		EnergyGain:   20,
+		StanceDamage: 30,
+	})
 }
