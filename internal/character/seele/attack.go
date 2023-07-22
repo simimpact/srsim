20835144f341c3a4dd698045adf9f6e57c6db8cf
Ryan Older <ryanolder67@gmail.com>
2023-07-22 21:00:27 +0700

    add basic atk logic

--- internal/character/seele/attack.go
+++ internal/character/seele/attack.go
@@ -3,8 +3,30 @@ package seele
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
 )
 
-func (c *char) Attack(target key.TargetID, state info.ActionState) {
+const (
+	Normal key.Attack = "seele-normal"
+)
 
+var attackHits = []float64{0.3, 0.7}
+
+func (c *char) Attack(target key.TargetID, state info.ActionState) {
+	for i, hitRatio := range attackHits {
+		c.engine.Attack(info.Attack{
+			Key:        Normal,
+			HitIndex:   i,
+			Source:     c.id,
+			Targets:    []key.TargetID{target},
+			DamageType: model.DamageType_QUANTUM,
+			AttackType: model.AttackType_NORMAL,
+			BaseDamage: info.DamageMap{
+				model.DamageFormula_BY_ATK: atk[c.info.AttackLevelIndex()],
+			},
+			StanceDamage: 30.0,
+			EnergyGain:   20.0,
+			HitRatio:     hitRatio,
+		})
+	}
 }
