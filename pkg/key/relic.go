fb168e9523a6c425b38e36f8502eb103a012fe5d
Kyle <mail@kyle.fail>
2023-07-22 15:05:34 -0700

    Fixed cli shutdown on windows (#181)

--- pkg/key/relic.go
+++ pkg/key/relic.go
@@ -12,7 +12,7 @@ const (
 
 // Planar
 const (
-	BelobogOfTheArchitects Relic = "belobog-of-the-architects"
+	BelobogOfTheArchitects Relic = "belobog_of_the_architects"
 	SpaceSealingStation    Relic = "space_sealing_station"
 	InertSalsotto          Relic = "inert_salsotto"
 	TaliaKingdomOfBanditry Relic = "talia_kingdom_of_banditry"
