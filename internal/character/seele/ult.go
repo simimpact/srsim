fc89ccf3d22ff9caa609cd75fd2ab8d90112a531
Ryan Older <ryanolder67@gmail.com>
2023-07-23 12:43:25 +0700

    add ult logic

--- internal/character/seele/ult.go
+++ internal/character/seele/ult.go
@@ -2,9 +2,34 @@ package seele
 
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
+)
+
+const (
+	Ult key.Attack = "seele-ult"
 )
 
 func (c *char) Ult(target key.TargetID, state info.ActionState) {
+	// add buffedState mod
+	c.engine.AddModifier(c.id, info.Modifier{
+		Name:   BuffedState,
+		Source: c.id,
+		Stats:  info.PropMap{prop.AllDamagePercent: talent[c.info.TalentLevelIndex()]},
+	})
 
+	// deal ult dmg
+	c.engine.Attack(info.Attack{
+		Key:        Ult,
+		Source:     c.id,
+		Targets:    []key.TargetID{target},
+		DamageType: model.DamageType_QUANTUM,
+		AttackType: model.AttackType_ULT,
+		BaseDamage: info.DamageMap{
+			model.DamageFormula_BY_ATK: ult[c.info.UltLevelIndex()],
+		},
+		StanceDamage: 90.0,
+		EnergyGain:   5.0,
+	})
 }
