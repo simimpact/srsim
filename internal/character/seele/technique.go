c2ad7861be6b25f72bb9a8b3c8b000e8eb0127b6
Ryan Older <ryanolder67@gmail.com>
2023-07-24 16:04:01 +0700

    jank technique impl

--- internal/character/seele/technique.go
+++ internal/character/seele/technique.go
@@ -2,13 +2,38 @@ package seele
 
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
+)
+
+const (
+	Technique key.Attack = "seele-technique"
 )
 
 // After using her Technique, Seele gains Stealth for 20 second(s).
 // While Stealth is active, Seele cannot be detected by enemies.
 // And when entering battle by attacking enemies, Seele will immediately enter the buffed state.
 
-func (c *char) Technique(target key.TargetID, state info.ActionState) {
+// IMPL : ignore maze stealth. set to onBattleStart : add buffedState mod to seele.
 
+func (c *char) Technique(target key.TargetID, state info.ActionState) {
+	c.engine.AddModifier(c.id, info.Modifier{
+		Name:   BuffedState,
+		Source: c.id,
+		Stats:  info.PropMap{prop.AllDamagePercent: talent[c.info.TalentLevelIndex()]},
+	})
+	// add the minimal toughness damage atk? (observed effect, not in the DM?)
+	c.engine.Attack(info.Attack{
+		Key:        key.Attack(BuffedState),
+		Source:     c.id,
+		Targets:    c.engine.Enemies(),
+		DamageType: model.DamageType_QUANTUM,
+		AttackType: model.AttackType_MAZE,
+		BaseDamage: info.DamageMap{
+			model.DamageFormula_BY_ATK: 0.0,
+		},
+		StanceDamage: 60.0,
+		EnergyGain:   0.0,
+	})
 }
