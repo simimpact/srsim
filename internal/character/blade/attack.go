1da3e2de5cd7c60e12d39c3dbf1d45b69b8a8838
Kisa <janbeier0@gmail.com>
2023-07-25 14:44:10 +0200

    implemented attack and skill

--- internal/character/blade/attack.go
+++ internal/character/blade/attack.go
@@ -3,8 +3,64 @@ package blade
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
 )
 
-func (c *char) Attack(target key.TargetID, state info.ActionState) {
+const Normal key.Attack = "blade-normal"
+const EnhancedNormal key.Attack = "blade-enhanced-normal"
 
+var attackHits = []float64{0.5, 0.5}
+
+func (c *char) Attack(target key.TargetID, state info.ActionState) {
+	if !c.engine.HasModifier(c.id, Hellscape) {
+		for i, hitRatio := range attackHits {
+			c.engine.Attack(info.Attack{
+				Key:        Normal,
+				HitIndex:   i,
+				Source:     c.id,
+				Targets:    []key.TargetID{target},
+				DamageType: model.DamageType_WIND,
+				AttackType: model.AttackType_NORMAL,
+				BaseDamage: info.DamageMap{
+					model.DamageFormula_BY_ATK: atk[c.info.AttackLevelIndex()],
+				},
+				StanceDamage: 30.0,
+				EnergyGain:   20.0,
+				HitRatio:     hitRatio,
+			})
+		}
+	} else {
+		// Primary Target
+		for i, hitRatio := range attackHits {
+			c.engine.Attack(info.Attack{
+				Key:        EnhancedNormal,
+				HitIndex:   i,
+				Source:     c.id,
+				Targets:    []key.TargetID{target},
+				DamageType: model.DamageType_WIND,
+				AttackType: model.AttackType_NORMAL,
+				BaseDamage: info.DamageMap{
+					model.DamageFormula_BY_ATK:    enhancedBasicSingleAtk[c.info.AttackLevelIndex()],
+					model.DamageFormula_BY_MAX_HP: enhancedBasicSingleHP[c.info.AttackLevelIndex()],
+				},
+				StanceDamage: 60.0,
+				EnergyGain:   30.0,
+				HitRatio:     hitRatio,
+			})
+		}
+
+		// Adjacent Targets
+		c.engine.Attack(info.Attack{
+			Key:        EnhancedNormal,
+			Source:     c.id,
+			Targets:    c.engine.AdjacentTo(target),
+			DamageType: model.DamageType_WIND,
+			AttackType: model.AttackType_NORMAL,
+			BaseDamage: info.DamageMap{
+				model.DamageFormula_BY_ATK:    enhancedBasicBlastAtk[c.info.AttackLevelIndex()],
+				model.DamageFormula_BY_MAX_HP: enhancedBasicBlastHP[c.info.AttackLevelIndex()],
+			},
+			StanceDamage: 30.0,
+		})
+	}
 }
--- internal/character/blade/skill.go
+++ internal/character/blade/skill.go
@@ -2,9 +2,34 @@ package blade
 
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
+)
+
+const (
+	Hellscape key.Modifier = "blade-hellscape"
 )
 
 func (c *char) Skill(target key.TargetID, state info.ActionState) {
+	if c.engine.HasModifier(c.id, Hellscape) {
+		c.engine.ModifyHPByRatio(info.ModifyHPByRatio{
+			Key:       key.Reason(Hellscape),
+			Target:    c.id,
+			Source:    c.id,
+			Ratio:     -0.3,
+			RatioType: model.ModifyHPRatioType_MAX_HP,
+			Floor:     1,
+		})
 
+		// TODO: Duration Behaviour?
+		c.engine.AddModifier(c.id, info.Modifier{
+			Name:     Hellscape,
+			Source:   c.id,
+			Stats:    info.PropMap{prop.AllDamagePercent: skill[c.info.SkillLevelIndex()]},
+			Duration: 3,
+		})
+
+		c.engine.InsertAction(c.id)
+	}
 }
