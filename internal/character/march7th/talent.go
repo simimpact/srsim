e4b323616f0abcb70afbfd4720c267bda518f15b
aaron-hwang <aaronhwang22@gmail.com>
2023-07-26 16:22:55 -0400

    Implemented skeleton for Talent

--- internal/character/march7th/talent.go
+++ internal/character/march7th/talent.go
@@ -1,1 +1,22 @@
 package march7th
+
+import "github.com/simimpact/srsim/pkg/engine/info"
+
+const (
+	Talent = "march7th-talent"
+)
+
+func init() {
+
+}
+
+func (c *char) initTalent() {
+	c.engine.AddModifier(c.id, info.Modifier{
+		Name:   Talent,
+		Source: c.id,
+	})
+}
+
+func (c *char) talentCounterListener() {
+
+}
