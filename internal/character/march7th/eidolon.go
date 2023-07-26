3b2eb79f0f1fc710ad6b76317a7b312d0b08aca4
aaron-hwang <aaronhwang22@gmail.com>
2023-07-26 16:22:32 -0400

    Added skeleton for Eidolons

--- internal/character/march7th/eidolon.go
+++ internal/character/march7th/eidolon.go
@@ -1,1 +1,19 @@
 package march7th
+
+import (
+	"github.com/simimpact/srsim/pkg/key"
+)
+
+const ()
+
+func init() {
+
+}
+
+func (c *char) initEidolons() {
+
+}
+
+func (c *char) e2(target key.TargetID) {
+
+}
