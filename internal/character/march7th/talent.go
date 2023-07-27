2c201f25558198097f633a84721007b59548405c
aaron-hwang <aaronhwang22@gmail.com>
2023-07-26 23:43:54 -0400

    Added parts of skeleton

--- internal/character/march7th/talent.go
+++ internal/character/march7th/talent.go
@@ -1,12 +1,79 @@
 package march7th
 
-import "github.com/simimpact/srsim/pkg/engine/info"
-
-const (
-	Talent = "march7th-talent"
+import (
+	"github.com/simimpact/srsim/pkg/engine/event"
+	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/modifier"
+	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
 )
 
+const (
+	Talent             = "march7th-talent"
+	TalentCount        = "march7th-talentcount"
+	MarchAllyMark      = "march7th-shield-counter"
+	MarchCounterMark   = "march7th-counter-mark"
+	MarchCounterAttack = "march7th-counter"
+)
+
+type counterState struct {
+	countersLeft   int
+	counterScaling float64
+}
+
 func init() {
+	modifier.Register(MarchCounterMark, modifier.Config{
+		Listeners: modifier.Listeners{
+			OnAfterAttack: func(mod *modifier.Instance, e event.AttackEnd) {
+				mod.Engine().InsertAbility(info.Insert{
+					Key: MarchCounterAttack,
+					Execute: func() {
+						mod.Engine().Attack(info.Attack{
+							Source:     mod.Source(),
+							Targets:    []key.TargetID{mod.Owner()},
+							AttackType: model.AttackType_INSERT,
+							DamageType: model.DamageType_ICE,
+						})
+					},
+					AbortFlags: []model.BehaviorFlag{
+						model.BehaviorFlag_DISABLE_ACTION,
+						model.BehaviorFlag_STAT_CTRL,
+					},
+				})
+			},
+		},
+	})
+
+	modifier.Register(MarchAllyMark, modifier.Config{
+		Listeners: modifier.Listeners{
+			OnBeforeBeingAttacked: func(mod *modifier.Instance, e event.AttackStart) {
+
+				/*oneTargetWasShielded := false
+				for _, target := range e.Targets {
+					//oneTargetWasShielded = mod.Engine().HasShield(target, key.Shield()) || oneTargetWasShielded
+				}*/
+			},
+		},
+	})
+
+	modifier.Register(Talent, modifier.Config{
+		Listeners: modifier.Listeners{
+			OnAdd: func(mod *modifier.Instance) {
+				for _, teammate := range mod.Engine().Characters() {
+					mod.Engine().AddModifier(teammate, info.Modifier{
+						Name:   MarchAllyMark,
+						Source: mod.Owner(),
+					})
+				}
+			},
+			OnPhase2: func(mod *modifier.Instance) {
+				mod.Engine().AddModifier(mod.Owner(), info.Modifier{
+					Name: TalentCount,
+				})
+
+			},
+		},
+	})
 
 }
 
@@ -17,6 +84,6 @@ func (c *char) initTalent() {
 	})
 }
 
-func (c *char) talentCounterListener() {
+func (c *char) talentCounterListener(e event.CharactersAdded) {
 
 }
