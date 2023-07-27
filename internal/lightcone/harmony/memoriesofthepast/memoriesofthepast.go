0ed9124a4ce207b283857b4e698d10ae7ccf5e7c
Ryan Older <ryanolder67@gmail.com>
2023-07-27 21:21:53 +0700

    fill onCreate logic

--- internal/lightcone/harmony/memoriesofthepast/memoriesofthepast.go
+++ internal/lightcone/harmony/memoriesofthepast/memoriesofthepast.go
@@ -6,6 +6,7 @@ import (
 	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/engine/modifier"
+	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
 )
@@ -27,15 +28,30 @@ func init() {
 	modifier.Register(memories, modifier.Config{
 		Listeners: modifier.Listeners{
 			OnAfterAttack: addEnergy,
+			// DM uses OnListenTurnEnd. try to imitate with normal and skill action check.
+			OnAfterAction: trackCooldown,
 		},
 	})
 }
 
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
+	beAmt := 0.21 + 0.07*float64(lc.Imposition)
+	energyAmt := 3.0 + 1.0*float64(lc.Imposition)
 
+	engine.AddModifier(owner, info.Modifier{
+		Name:   memories,
+		Source: owner,
+		Stats:  info.PropMap{prop.BreakEffect: beAmt},
+		State:  energyAmt,
+	})
 }
 
 // add energy on attack IF not on cd.
 func addEnergy(mod *modifier.Instance, e event.AttackEnd) {
+
+}
+
+// if action = basic/skill, reset cd.
+func trackCooldown(mod *modifier.Instance, e event.ActionEnd) {
 
 }
