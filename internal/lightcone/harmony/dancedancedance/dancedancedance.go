acc26a81f50161e8b7c927258130518a23655347
Ryan Older <ryanolder67@gmail.com>
2023-07-28 14:12:23 +0700

    add logics

--- internal/lightcone/harmony/dancedancedance/dancedancedance.go
+++ internal/lightcone/harmony/dancedancedance/dancedancedance.go
@@ -3,9 +3,15 @@ package dancedancedance
 import (
 	"github.com/simimpact/srsim/pkg/engine"
 	"github.com/simimpact/srsim/pkg/engine/equip/lightcone"
+	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/modifier"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
+)
+
+const (
+	dance = "dance-dance-dance"
 )
 
 // When the wearer uses their Ultimate, all allies' actions are Advanced Forward by 16%
@@ -17,8 +23,17 @@ func init() {
 		Path:          model.Path_HARMONY,
 		Promotions:    promotions,
 	})
+	modifier.Register(dance, modifier.Config{
+		Listeners: modifier.Listeners{
+			OnAfterAction: advForwardOnUlt,
+		},
+	})
 }
 
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
+
+}
+
+func advForwardOnUlt(mod *modifier.Instance, e event.ActionEnd) {
 
 }
