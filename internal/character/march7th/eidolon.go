d6356d2cd5490ad10976278e033040911fda71d1
aaron-hwang <aaronhwang22@gmail.com>
2023-07-29 00:34:55 -0400

    Added start of implementation of e1

--- internal/character/march7th/eidolon.go
+++ internal/character/march7th/eidolon.go
@@ -1,10 +1,13 @@
 package march7th
 
 import (
+	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/key"
 )
 
-const ()
+const (
+	MarchE2 key.Reason = "march7th-e2"
+)
 
 func init() {
 
@@ -12,6 +15,19 @@ func init() {
 
 func (c *char) initEidolons() {
 
+}
+
+func (c *char) e1() {
+	for _, target := range c.engine.Enemies() {
+		if c.engine.HasModifier(target, MarchUltFreeze) {
+			c.engine.ModifyEnergy(info.ModifyAttribute{
+				Key:    MarchE2,
+				Source: c.id,
+				Target: target,
+				Amount: 5,
+			})
+		}
+	}
 }
 
 func (c *char) e2(target key.TargetID) {
