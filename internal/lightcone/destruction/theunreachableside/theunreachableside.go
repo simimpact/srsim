7ded3744be392a092b432a66b0d90e5d2e6f3151
Ryan Older <ryanolder67@gmail.com>
2023-08-09 14:55:26 +0700

    init mod and listeners

--- internal/lightcone/destruction/theunreachableside/theunreachableside.go
+++ internal/lightcone/destruction/theunreachableside/theunreachableside.go
@@ -3,9 +3,15 @@ package theunreachableside
 import (
 	"github.com/simimpact/srsim/pkg/engine"
 	"github.com/simimpact/srsim/pkg/engine/equip/lightcone"
+	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/modifier"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
+)
+
+const (
+	side key.Modifier = "the-unreachable-side"
 )
 
 // Increases the wearer's CRIT rate by 18% and increases their Max HP by 18%.
@@ -19,8 +25,27 @@ func init() {
 		Path:          model.Path_DESTRUCTION,
 		Promotions:    promotions,
 	})
+	modifier.Register(side, modifier.Config{
+		Listeners: modifier.Listeners{
+			OnHPChange:           buffOnHPConsume,
+			OnAfterBeingAttacked: buffOnAttacked,
+			OnAfterAttack:        removeBuff,
+		},
+	})
 }
 
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
+
+}
+
+func buffOnHPConsume(mod *modifier.Instance, e event.HPChange) {
+
+}
+
+func buffOnAttacked(mod *modifier.Instance, e event.AttackEnd) {
+
+}
+
+func removeBuff(mod *modifier.Instance, e event.AttackEnd) {
 
 }
