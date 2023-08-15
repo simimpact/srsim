214d3514f91e32275ccc0bd0ed19dbf87a8f46b6
Ryan Older <ryanolder67@gmail.com>
2023-08-15 19:15:32 +0700

    patience lc : modifiers init

--- internal/lightcone/nihility/patienceisallyouneed/patienceisallyouneed.go
+++ internal/lightcone/nihility/patienceisallyouneed/patienceisallyouneed.go
@@ -3,9 +3,17 @@ package patienceisallyouneed
 import (
 	"github.com/simimpact/srsim/pkg/engine"
 	"github.com/simimpact/srsim/pkg/engine/equip/lightcone"
+	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/modifier"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
+)
+
+const (
+	patience key.Modifier = "patience-is-all-you-need"
+	spdBuff  key.Modifier = "patience-is-all-you-need-spd-buff"
+	erode                 = "patience-is-all-you-need-erode"
 )
 
 // Increases DMG dealt by the wearer by 24%. After every attack launched by wearer,
@@ -21,8 +29,41 @@ func init() {
 		Path:          model.Path_NIHILITY,
 		Promotions:    promotions,
 	})
+	modifier.Register(patience, modifier.Config{
+		Listeners: modifier.Listeners{
+			OnAfterHit:    inflictErode,
+			OnAfterAttack: addSpeedBuff,
+		},
+	})
+	modifier.Register(spdBuff, modifier.Config{
+		BehaviorFlags: []model.BehaviorFlag{
+			model.BehaviorFlag_STAT_SPEED_UP,
+		},
+		Stacking: modifier.ReplaceBySource,
+	})
+	modifier.Register(erode, modifier.Config{
+		Listeners: modifier.Listeners{
+			OnPhase1: addDotDmg,
+		},
+		BehaviorFlags: []model.BehaviorFlag{
+			model.BehaviorFlag_STAT_DOT_ELECTRIC,
+		},
+		Stacking: modifier.ReplaceBySource,
+	})
 }
 
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
+
+}
+
+func addSpeedBuff(mod *modifier.Instance, e event.AttackEnd) {
+
+}
+
+func inflictErode(mod *modifier.Instance, e event.HitEnd) {
+
+}
+
+func addDotDmg(mod *modifier.Instance) {
 
 }
