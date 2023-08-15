31b6bdc2c9828bca536eacab408f69c33191d35c
Ryan Older <ryanolder67@gmail.com>
2023-08-15 17:39:57 +0700

    void lc : register modifier

--- internal/lightcone/nihility/void/void.go
+++ internal/lightcone/nihility/void/void.go
@@ -4,8 +4,13 @@ import (
 	"github.com/simimpact/srsim/pkg/engine"
 	"github.com/simimpact/srsim/pkg/engine/equip/lightcone"
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/modifier"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
+)
+
+const (
+	ehrBuff key.Modifier = "void"
 )
 
 // At the start of the battle, the wearer's Effect Hit Rate increases by 20% for 3 turn(s).
@@ -17,6 +22,7 @@ func init() {
 		Path:          model.Path_NIHILITY,
 		Promotions:    promotions,
 	})
+	modifier.Register(ehrBuff, modifier.Config{})
 }
 
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
