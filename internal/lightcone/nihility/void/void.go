5e3c81e4a00d4275814057871b4127ec705d9e3e
Ryan Older <ryanolder67@gmail.com>
2023-08-15 17:50:15 +0700

    void lc : finish event subscriber logic

--- internal/lightcone/nihility/void/void.go
+++ internal/lightcone/nihility/void/void.go
@@ -3,8 +3,10 @@ package void
 import (
 	"github.com/simimpact/srsim/pkg/engine"
 	"github.com/simimpact/srsim/pkg/engine/equip/lightcone"
+	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/engine/modifier"
+	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
 )
@@ -26,5 +28,14 @@ func init() {
 }
 
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
-
+	ehrAmt := 0.15 + 0.05*float64(lc.Imposition)
+	// onBattleStart : add ehr buff.
+	engine.Events().BattleStart.Subscribe(func(event event.BattleStart) {
+		engine.AddModifier(owner, info.Modifier{
+			Name:     ehrBuff,
+			Source:   owner,
+			Stats:    info.PropMap{prop.EffectHitRate: ehrAmt},
+			Duration: 3,
+		})
+	})
 }
