6f2ba06774b88534acc51628924b1b27fb36fa1a
Gort <126199575+GortFan@users.noreply.github.com>
2023-08-17 13:26:36 -0400

    Implement Shields [Core] (#154)
    * Implement Something Irreplaceable [LC]
    
    * fix issue
    
    * Fixed buff application logic
    
    * Implement Seriousness of Breakfast [LC]
    
    * Fix Seriousness of Breakfast LC stacking & naming
    
    * Implement Under the Blue Sky [LC]
    
    * Fix buff naming
    
    * Added replace stacking to refresh duration of buff
    
    * Implement Collapsing Sky [LC]
    
    * ?
    
    * Implement Collapsing Sky [LC]
    
    * Merge branch 'simimpact:main' into main
    
    * Implement On the Fall of an Aeon [LC]
    
    * Implement Shields
    
    * Modify shield add
    
    * Lint check issue fix
    
    * Shield Implementation Fixes
    
    * Shield Implementation Fixes
    
    * revert changes to vscode settings file
    
    * Address CR comments
    
    * Address nits from CR

--- pkg/engine/shield/absorb.go
+++ pkg/engine/shield/absorb.go
@@ -1,37 +1,63 @@
 package shield
 
 import (
+	"math"
+
 	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/key"
 )
 
 func (mgr *Manager) AbsorbDamage(target key.TargetID, damage float64) float64 {
-	// TODO: ACTUAL SHIELD LOGIC GOES HERE
-	// 1. for every shield on this target, attempt to deal "damage" amount of damage to the shield
-	// 2. for each shield, a ShieldChangeEvent should be emitted (how much damage was done to the shield)
-	// 3. if shield HP <= damage, remove that shield (mgr.RemoveShield to generate event)
-	// 4. if all shields HP <= damage, return will be damage - max(ShieldHP) [the damage not blocked by shield]
-	// 5. if there are shields which HP > damage, return should be 0
-	// 6. remaining state should be only shields that starting HP > damage
-
-	// placeholder just for some basic event emission
-	for id, shield := range mgr.targets[target] {
-		prevHP := shield.HP
-		shield.HP -= damage
-		if shield.HP < 0 {
-			shield.HP = 0
-		}
-		mgr.event.ShieldChange.Emit(event.ShieldChange{
-			ID:     id,
-			Target: target,
-			OldHP:  prevHP,
-			NewHP:  shield.HP,
-		})
-
-		if shield.HP == 0 {
-			mgr.RemoveShield(id, target)
-		}
+	// Check if shielded or damage is non-positive
+	if !mgr.IsShielded(target) || damage <= 0 {
+		return damage
 	}
 
-	return damage
+	var removedShields []*Instance
+	i := 0
+	damageOut := damage
+	newMaxShieldHP := 0.0
+	oldMaxShieldHP := mgr.MaxShield(target)
+	var maxShieldID key.Shield
+	// Apply damage to all shields attached to entity
+	for _, shield := range mgr.targets[target] {
+		remaining := math.Dim(damage, shield.hp)
+		shield.hp = math.Dim(shield.hp, damage)
+		// Outgoing damage is always the lowest damage remaining after initial damage is subtracted from shields
+		if remaining < damageOut {
+			damageOut = remaining
+		}
+		// Max shield is always the highest shield hp remaining after damage is applied
+		if shield.hp > newMaxShieldHP {
+			newMaxShieldHP = shield.hp
+			maxShieldID = shield.name
+		}
+		// Generate list of shields to remove
+		if shield.hp == 0 {
+			removedShields = append(removedShields, shield)
+		} else {
+			mgr.targets[target][i] = shield
+			i++
+		}
+	}
+	mgr.targets[target] = mgr.targets[target][:i]
+
+	// Event emission to remove shields that have 0 hp
+	for _, shield := range removedShields {
+		mgr.event.ShieldRemoved.Emit(event.ShieldRemoved{
+			ID:     shield.name,
+			Target: target,
+		})
+	}
+
+	// Event emission for shield hp change
+	mgr.event.ShieldChange.Emit(event.ShieldChange{
+		Target:    target,
+		ID:        maxShieldID,
+		OldHP:     oldMaxShieldHP,
+		NewHP:     newMaxShieldHP,
+		DamageIn:  damage,
+		DamageOut: damageOut,
+	})
+	return damageOut
 }
--- pkg/engine/shield/remove.go
+++ pkg/engine/shield/remove.go
@@ -11,7 +11,14 @@ func (mgr *Manager) RemoveShield(id key.Shield, target key.TargetID) {
 		return
 	}
 
-	delete(mgr.targets[target], id)
+	i := 0
+	for _, shield := range mgr.targets[target] {
+		if shield.name != id {
+			mgr.targets[target][i] = shield
+			i++
+		}
+	}
+	mgr.targets[target] = mgr.targets[target][:i]
 
 	mgr.event.ShieldRemoved.Emit(event.ShieldRemoved{
 		ID:     id,
