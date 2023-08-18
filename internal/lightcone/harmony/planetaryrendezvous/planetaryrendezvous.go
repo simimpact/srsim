e81873f92afbe4fc8381656432cf0ed0837842dd
Ryan Older <ryanolder67@gmail.com>
2023-08-18 08:58:39 +0700

    simplify. use prop.dmg% to map dmgtype to dmg%

--- internal/lightcone/harmony/planetaryrendezvous/planetaryrendezvous.go
+++ internal/lightcone/harmony/planetaryrendezvous/planetaryrendezvous.go
@@ -30,30 +30,16 @@ func init() {
 
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
 	dmgAmt := 0.09 + 0.03*float64(lc.Imposition)
-	// init elements list. use maps for more compact code.
-	statsMap := map[model.DamageType]info.PropMap{
-		model.DamageType_FIRE:      {prop.FireDamagePercent: dmgAmt},
-		model.DamageType_ICE:       {prop.IceDamagePercent: dmgAmt},
-		model.DamageType_IMAGINARY: {prop.ImaginaryDamagePercent: dmgAmt},
-		model.DamageType_THUNDER:   {prop.ThunderDamagePercent: dmgAmt},
-		model.DamageType_PHYSICAL:  {prop.PhysicalDamagePercent: dmgAmt},
-		model.DamageType_QUANTUM:   {prop.QuantumDamagePercent: dmgAmt},
-		model.DamageType_WIND:      {prop.WindDamagePercent: dmgAmt},
-	}
 
 	engine.Events().BattleStart.Subscribe(func(event event.BattleStart) {
 		holderInfo, _ := engine.CharacterInfo(owner)
 		for _, char := range engine.Characters() {
-			// check current char element against lc holder
-			charInfo, _ := engine.CharacterInfo(char)
-			if charInfo.Element == holderInfo.Element {
-				// if element matches. add perm element specific dmg buff.
-				engine.AddModifier(char, info.Modifier{
-					Name:   dmgBuff,
-					Source: owner,
-					Stats:  statsMap[holderInfo.Element],
-				})
-			}
+			// add lcholder's element's buff to all chars
+			engine.AddModifier(char, info.Modifier{
+				Name:   dmgBuff,
+				Source: owner,
+				Stats:  info.PropMap{prop.DamagePercent(holderInfo.Element): dmgAmt},
+			})
 		}
 	})
 }
