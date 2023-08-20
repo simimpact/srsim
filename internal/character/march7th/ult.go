ed7277136159e2cfd0cfb15a039814cb18133568
aaron-hwang <aaronhwang22@gmail.com>
2023-08-20 00:10:49 -0400

    Changed ult implementation

--- internal/character/march7th/ult.go
+++ internal/character/march7th/ult.go
@@ -1,6 +1,7 @@
 package march7th
 
 import (
+	"github.com/simimpact/srsim/internal/global/common"
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/engine/modifier"
 	"github.com/simimpact/srsim/pkg/key"
@@ -21,17 +22,63 @@ func init() {
 var ultHits = []float64{0.25, 0.25, 0.25, 0.25}
 
 func (c *char) Ult(target key.TargetID, state info.ActionState) {
-	targets := c.engine.Enemies()
 
-	for i, hitRatio := range ultHits {
+	for hitIndex, hitRatio := range ultHits {
+		c.ultHit(hitIndex, hitRatio)
+	}
+
+	c.engine.EndAttack()
+
+	freezeChance := 0.5
+	if c.info.Traces["103"] {
+		freezeChance += 0.15
+	}
+
+	for _, freezeTarget := range c.engine.Enemies() {
+		successfullyApplied, _ := c.engine.AddModifier(freezeTarget, info.Modifier{
+			Name:   common.Freeze,
+			Source: c.id,
+			State: common.FreezeState{
+				DamagePercentage: ultFreeze[c.info.UltLevelIndex()],
+			},
+			Chance:   freezeChance,
+			Duration: 1,
+		})
+
+		if c.info.Eidolon >= 1 {
+			if successfullyApplied {
+				c.engine.ModifyEnergy(info.ModifyAttribute{
+					Key:    Ult,
+					Target: c.id,
+					Source: c.id,
+					Amount: 6,
+				})
+			}
+		}
+	}
+
+}
+
+func (c *char) ultHit(hitIndex int, hitRatio float64) {
+	hitTargets := make(map[key.TargetID]bool)
+	for i := 0; i < 2; i++ {
+		targets := c.engine.Retarget(info.Retarget{
+			Targets: c.engine.Enemies(),
+			Filter: func(target key.TargetID) bool {
+				_, beenHit := hitTargets[target]
+				return !beenHit
+			},
+			Max: 2,
+		})
+
 		c.engine.Attack(info.Attack{
 			Key:          Ult,
 			Source:       c.id,
 			Targets:      targets,
-			HitIndex:     i,
-			HitRatio:     hitRatio,
+			HitIndex:     hitIndex,
+			HitRatio:     hitRatio / 3,
 			StanceDamage: 60,
-			EnergyGain:   5,
+			EnergyGain:   0,
 			DamageType:   model.DamageType_ICE,
 			AttackType:   model.AttackType_ULT,
 			BaseDamage: info.DamageMap{
@@ -40,18 +87,16 @@ func (c *char) Ult(target key.TargetID, state info.ActionState) {
 		})
 	}
 
-	freezeChance := 0.5
-	if c.info.Traces["103"] {
-		freezeChance += 0.15
+	c.engine.ModifyEnergy(info.ModifyAttribute{
+		Key:    Ult,
+		Target: c.id,
+		Source: c.id,
+		Amount: 5 * 0.25,
+	})
+
+	//Unnecessary?
+	for t := range hitTargets {
+		delete(hitTargets, t)
 	}
 
-	for _, freezeTarget := range targets {
-		c.engine.AddModifier(freezeTarget, info.Modifier{
-			Name:     MarchUltFreeze,
-			Source:   c.id,
-			Chance:   freezeChance,
-			Duration: 1,
-		})
-	}
-	c.e1()
 }
