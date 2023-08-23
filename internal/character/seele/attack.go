8a579b86b48958b57462bca4305e370091604e16
Ryan Older <ryanolder67@gmail.com>
2023-08-23 10:12:23 +0700

    minor fixes

--- internal/character/seele/attack.go
+++ internal/character/seele/attack.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	Normal key.Attack = "seele-normal"
-	A2     key.Reason = "seele-a2"
+	A6     key.Reason = "seele-a6"
 )
 
 var attackHits = []float64{0.3, 0.7}
@@ -30,12 +30,16 @@ func (c *char) Attack(target key.TargetID, state info.ActionState) {
 			HitRatio:     hitRatio,
 		})
 	}
+	// call attackEnd
+	state.EndAttack()
 	// A6 : After using a Basic ATK, Seele's next action will be Advanced Forward by 20%.
-	if !c.info.Traces["103"] {
+	if !c.info.Traces["103"] { // NOTE : flip this (?)
 		return
 	}
+	// if not insert : modifygaugecost -20%
+	// if insert : modifynormalized -20%
 	c.engine.ModifyCurrentGaugeCost(info.ModifyCurrentGaugeCost{
-		Key:    A2,
+		Key:    A6,
 		Source: c.id,
 		Amount: -0.2,
 	})
