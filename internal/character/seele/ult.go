97ac36b21f89cbe3901c90b213ae433c781d3cc3
Ryan Older <ryanolder67@gmail.com>
2023-08-25 00:23:27 +0700

    implement new Resurgence func for Ult

--- internal/character/seele/ult.go
+++ internal/character/seele/ult.go
@@ -2,7 +2,6 @@ package seele
 
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
-	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
 )
@@ -13,20 +12,9 @@ const (
 
 // Enters the buffed state and deals Quantum DMG equal to 425% of her ATK to a single enemy.
 func (c *char) Ult(target key.TargetID, state info.ActionState) {
-	// A4 : While Seele is in the buffed state, her Quantum RES PEN increases by 20%.
-	penAmt := 0.0
-	if c.info.Traces["102"] {
-		penAmt = 0.2
-	}
-	c.engine.AddModifier(c.id, info.Modifier{
-		Name:   Resurgence,
-		Source: c.id,
-		Stats: info.PropMap{
-			prop.AllDamagePercent: talent[c.info.TalentLevelIndex()],
-			prop.QuantumPEN:       penAmt,
-		},
-	})
-
+	// enter buffed state w/o extra turns.
+	c.enterResurgence(false)
+	// ult attack.
 	c.engine.Attack(info.Attack{
 		Key:        Ult,
 		Source:     c.id,
