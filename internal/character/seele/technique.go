bc56dd047d82333a8eefb2239e84bb4049dbf1af
Ryan Older <ryanolder67@gmail.com>
2023-08-25 00:27:45 +0700

    implement resurgence func on technique

--- internal/character/seele/technique.go
+++ internal/character/seele/technique.go
@@ -2,7 +2,6 @@ package seele
 
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
-	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
 )
 
@@ -15,17 +14,6 @@ const (
 // And when entering battle by attacking enemies, Seele will immediately enter the buffed state.
 
 func (c *char) Technique(target key.TargetID, state info.ActionState) {
-	// A4 : While Seele is in the buffed state, her Quantum RES PEN increases by 20%.
-	resPenAmt := 0.0
-	if c.info.Traces["102"] {
-		resPenAmt = 0.2
-	}
-	c.engine.AddModifier(c.id, info.Modifier{
-		Name:   BuffedState,
-		Source: c.id,
-		Stats: info.PropMap{
-			prop.AllDamagePercent: talent[c.info.TalentLevelIndex()],
-			prop.QuantumPEN:       resPenAmt,
-		},
-	})
+	// enter buffed state w/o adding extra turns
+	c.enterResurgence(false)
 }
