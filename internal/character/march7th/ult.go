37bf10d64c472251c2d18c5096f4c5292b1decba
aaron-hwang <aaronhwang22@gmail.com>
2023-08-25 15:58:33 -0400

    Style

--- internal/character/march7th/ult.go
+++ internal/character/march7th/ult.go
@@ -22,7 +22,6 @@ func init() {
 var ultHits = []float64{0.25, 0.25, 0.25, 0.25}
 
 func (c *char) Ult(target key.TargetID, state info.ActionState) {
-
 	for hitIndex, hitRatio := range ultHits {
 		c.ultHit(hitIndex, hitRatio)
 	}
