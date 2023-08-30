8d3500b2ee512a833105691deb7b253f158ed34b
Ryan Older <ryanolder67@gmail.com>
2023-08-31 05:58:03 +0700

    add skill logics

--- internal/character/bailu/skill.go
+++ internal/character/bailu/skill.go
@@ -3,8 +3,49 @@ package bailu
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
 )
 
-func (c *char) Skill(target key.TargetID, state info.ActionState) {
+const (
+	Skill = "bailu-skill"
+)
 
+// Heals a single ally for 11.7% of Bailu's Max HP plus 312.
+// Bailu then heals random allies 2 time(s). After each healing,
+// HP restored from the next healing is reduced by 15%.
+
+func (c *char) Skill(target key.TargetID, state info.ActionState) {
+	// record heal amt for next 2 random heals.
+	healPercent := skillPercent[c.info.SkillLevelIndex()]
+	healFlat := skillFlat[c.info.SkillLevelIndex()]
+	// main targeted heal
+	c.engine.Heal(info.Heal{
+		Key:     Skill,
+		Source:  c.id,
+		Targets: []key.TargetID{target},
+		BaseHeal: info.HealMap{
+			model.HealFormula_BY_HEALER_MAX_HP: healPercent,
+		},
+		HealValue: healFlat,
+	})
+
+	// 2 randomized heals
+	for i := 0; i < 2; i++ {
+		// reduce heal amt for each subsequent heals
+		healPercent = 0.85 * healPercent
+		healFlat = 0.85 * healFlat
+		chosenTarget := c.engine.Retarget(info.Retarget{
+			Targets: c.engine.Characters(),
+			Max:     1,
+		})
+		c.engine.Heal(info.Heal{
+			Key:     Skill,
+			Source:  c.id,
+			Targets: chosenTarget,
+			BaseHeal: info.HealMap{
+				model.HealFormula_BY_HEALER_MAX_HP: healPercent,
+			},
+			HealValue: healFlat,
+		})
+	}
 }
