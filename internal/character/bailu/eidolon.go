58a3c237920ec7eb53f98f2708504c2a0b15a024
Ryan Older <ryanolder67@gmail.com>
2023-09-04 06:56:30 +0700

    init eidolons

---
+++ internal/character/bailu/eidolon.go
@@ -0,0 +1,17 @@
+package bailu
+
+// E1 : If the target ally's current HP is equal to their Max HP when Invigoration ends,
+//      regenerates 8 extra Energy for this target.
+// E2 : After using her Ultimate, Bailu's Outgoing Healing increases
+//      by an additional 15% for 2 turn(s).
+// E4 : Every healing provided by the Skill makes the recipient deal 10% more DMG for 2 turn(s).
+//      This effect can stack up to 3 time(s).
+// E6 : Bailu can heal allies who received a killing blow 1 more time(s) in a single battle.
+
+func init() {
+	// register
+}
+
+func (c *char) initEidolons() {
+	// add mods/set values here
+}
