0510bb9047201a39a9d0b6d9d4dde1062720d6b4
Ryan Older <ryanolder67@gmail.com>
2023-09-04 06:18:59 +0700

    add technique logics

--- internal/character/bailu/technique.go
+++ internal/character/bailu/technique.go
@@ -5,6 +5,11 @@ import (
 	"github.com/simimpact/srsim/pkg/key"
 )
 
-func (c *char) Technique(target key.TargetID, state info.ActionState) {
+// After Technique is used, at the start of the next battle,
+// all allies are granted Invigoration for 2 turn(s).
 
+func (c *char) Technique(target key.TargetID, state info.ActionState) {
+	for _, char := range c.engine.Characters() {
+		c.addInvigoration(char, 2)
+	}
 }
