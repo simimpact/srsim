1ba9d8c73e627c5125cdeb4f6ece70cd4d64dcae
Ryan Older <ryanolder67@gmail.com>
2023-09-04 06:23:22 +0700

    init trace logics

---
+++ internal/character/bailu/trace.go
@@ -0,0 +1,14 @@
+package bailu
+
+// A2 : When Bailu heals a target ally above their normal Max HP,
+//      the target's Max HP increases by 10% for 2 turns.
+// A4 : Invigoration can trigger 1 more time(s).
+// A6 : Characters with Invigoration receive 10% less DMG.
+
+func init() {
+	// register here
+}
+
+func (c *char) initTraces() {
+	// add self modifiers here.
+}
