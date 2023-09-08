ae54dcfb22997fc84f1654203e05ce5200828362
Ryan Older <ryanolder67@gmail.com>
2023-09-08 14:07:02 +0700

    add attack logic

--- internal/character/welt/attack.go
+++ internal/character/welt/attack.go
@@ -3,8 +3,27 @@ package welt
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
 )
 
-func (c *char) Attack(target key.TargetID, state info.ActionState) {
+const (
+	Normal = "welt-normal"
+)
 
+// Deals Imaginary DMG equal to 100% of Welt's ATK to a single enemy.
+func (c *char) Attack(target key.TargetID, state info.ActionState) {
+	c.engine.Attack(info.Attack{
+		Key:        Normal,
+		Source:     c.id,
+		Targets:    []key.TargetID{target},
+		DamageType: model.DamageType_IMAGINARY,
+		AttackType: model.AttackType_NORMAL,
+		BaseDamage: info.DamageMap{
+			model.DamageFormula_BY_ATK: atk[c.info.AttackLevelIndex()],
+		},
+		StanceDamage: 30.0,
+		EnergyGain:   20.0,
+	})
+
+	state.EndAttack()
 }
