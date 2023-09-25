7ea90377649f14d92e40d8ac4a7faad2288c3639
aaron-hwang <aaronhwang22@gmail.com>
2023-09-24 22:19:00 -0400

    May need to split into 2 functions

--- internal/character/hook/talent.go
+++ internal/character/hook/talent.go
@@ -1,9 +1,9 @@
 package hook
 
 import (
-	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
-	"github.com/simimpact/srsim/pkg/engine/modifier"
+	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
 )
 
 const (
@@ -11,21 +11,47 @@ const (
 )
 
 func init() {
-	modifier.Register(Talent, modifier.Config{
-		Listeners: modifier.Listeners{
-			OnBeforeAttack: talentProc,
-		},
-	})
+
 }
 
 func (c *char) initTalent() {
-	c.engine.AddModifier(c.id, info.Modifier{
-		Name:   Talent,
-		Source: c.id,
-	})
+
 }
 
-func talentProc(mod *modifier.Instance, e event.AttackStart) {
-	mod.Engine().Attack(info.Attack{})
+// TODO: Split into 2 functions? 1 for actual proc and one for damage across indiscriminate amount of units?
+func (c *char) talentProc(target key.TargetID) {
+	c.engine.ModifyEnergy(info.ModifyAttribute{
+		Key:    Talent,
+		Target: c.id,
+		Source: c.id,
+		Amount: 5,
+	})
+
+	//Actual talent damage
+	c.engine.Attack(info.Attack{
+		Key:        Talent,
+		Source:     c.id,
+		Targets:    []key.TargetID{target},
+		AttackType: model.AttackType_PURSUED,
+		DamageType: model.DamageType_FIRE,
+		BaseDamage: info.DamageMap{
+			model.DamageFormula_BY_ATK: talent[c.info.TalentLevelIndex()],
+		},
+	})
+
+	if c.info.Eidolon >= 4 {
+		c.applySkillBurn(c.engine.AdjacentTo(target))
+	}
+
+	if c.info.Traces["101"] {
+		c.engine.Heal(info.Heal{
+			Key:     Talent,
+			Targets: []key.TargetID{c.id},
+			Source:  c.id,
+			BaseHeal: info.HealMap{
+				model.HealFormula_BY_HEALER_MAX_HP: 0.05,
+			},
+		})
+	}
 
 }
