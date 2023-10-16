5cdf2130bf06c6165a66f22d1c067c58bf83d86f
Ryan Older <ryanolder67@gmail.com>
2023-10-16 15:29:34 +0700

    A2 : merge return early conditions into 1

--- internal/character/bailu/trace.go
+++ internal/character/bailu/trace.go
@@ -24,16 +24,15 @@ func (c *char) initTraces() {
 	// A2 : When Bailu heals a target ally above their normal Max HP,
 	//      the target's Max HP increases by 10% for 2 turns.
 	c.engine.Events().HealEnd.Subscribe(func(e event.HealEnd) {
-		if !c.info.Traces["101"] {
+		// return early if no A2, healer isn't bailu, or overflow heal amt is 0 or less.
+		if !c.info.Traces["101"] || e.Healer != c.id || e.OverflowHealAmount <= 0 {
 			return
 		}
-		if e.Healer == c.id && e.OverflowHealAmount > 0 {
-			c.engine.AddModifier(e.Target, info.Modifier{
-				Name:     A2,
-				Source:   c.id,
-				Duration: 2,
-				Stats:    info.PropMap{prop.HPPercent: 0.1},
-			})
-		}
+		c.engine.AddModifier(e.Target, info.Modifier{
+			Name:     A2,
+			Source:   c.id,
+			Duration: 2,
+			Stats:    info.PropMap{prop.HPPercent: 0.1},
+		})
 	})
 }
