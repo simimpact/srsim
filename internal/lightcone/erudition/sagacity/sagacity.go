314d2791cf3e083f2a17c0e63b3f14b0db01a8fb
Ryan Older <ryanolder67@gmail.com>
2023-10-18 21:23:23 +0700

    sagacity : modifier register

--- internal/lightcone/erudition/sagacity/sagacity.go
+++ internal/lightcone/erudition/sagacity/sagacity.go
@@ -3,10 +3,18 @@ package sagacity
 import (
 	"github.com/simimpact/srsim/pkg/engine"
 	"github.com/simimpact/srsim/pkg/engine/equip/lightcone"
+	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/modifier"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
 )
+
+const (
+	sagacity key.Modifier = "sagacity"
+)
+
+// When the wearer uses their Ultimate, increases ATK by 24% for 2 turn(s).
 
 func init() {
 	lightcone.Register(key.Sagacity, lightcone.Config{
@@ -15,8 +23,17 @@ func init() {
 		Path:          model.Path_ERUDITION,
 		Promotions:    promotions,
 	})
+	modifier.Register(sagacity, modifier.Config{
+		Listeners: modifier.Listeners{
+			OnBeforeAction: buffAtkOnUlt,
+		},
+	})
 }
 
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
+
+}
+
+func buffAtkOnUlt(mod *modifier.Instance, e event.ActionStart) {
 
 }
