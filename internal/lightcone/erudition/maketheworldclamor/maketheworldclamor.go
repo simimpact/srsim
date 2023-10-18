f8039ee24fd0add71e13bffd2f92ab008678e76e
Ryan Older <ryanolder67@gmail.com>
2023-10-18 21:00:10 +0700

    add battlestart energy recov

--- internal/lightcone/erudition/maketheworldclamor/maketheworldclamor.go
+++ internal/lightcone/erudition/maketheworldclamor/maketheworldclamor.go
@@ -3,10 +3,18 @@ package maketheworldclamor
 import (
 	"github.com/simimpact/srsim/pkg/engine"
 	"github.com/simimpact/srsim/pkg/engine/equip/lightcone"
+	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
 )
+
+const (
+	clamor = "make-the-world-clamor"
+)
+
+// The wearer regenerates 20 Energy immediately upon entering battle,
+// and increases Ultimate DMG by 32%.
 
 func init() {
 	lightcone.Register(key.MaketheWorldClamor, lightcone.Config{
@@ -18,5 +26,12 @@ func init() {
 }
 
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
-
+	engine.Events().BattleStart.Subscribe(func(e event.BattleStart) {
+		engine.ModifyEnergy(info.ModifyAttribute{
+			Key:    clamor,
+			Target: owner,
+			Source: owner,
+			Amount: 20,
+		})
+	})
 }
