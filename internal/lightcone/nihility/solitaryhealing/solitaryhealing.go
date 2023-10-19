92f0a9294f682bd05e66872db0345f85c9dfdbb8
Ryan Older <ryanolder67@gmail.com>
2023-10-19 23:53:13 +0700

    add checker mod + static BE buff

--- internal/lightcone/nihility/solitaryhealing/solitaryhealing.go
+++ internal/lightcone/nihility/solitaryhealing/solitaryhealing.go
@@ -6,6 +6,7 @@ import (
 	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/engine/modifier"
+	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
 )
@@ -39,7 +40,18 @@ func init() {
 }
 
 func Create(engine engine.Engine, owner key.TargetID, lc info.LightCone) {
+	// apply ult checker + static BE buff
+	breakEffectAmt := 0.15 + 0.05*float64(lc.Imposition)
+	dotBuffAmt := 0.18 + 0.06*float64(lc.Imposition)
 
+	engine.AddModifier(owner, info.Modifier{
+		Name:   check,
+		Source: owner,
+		Stats:  info.PropMap{prop.BreakEffect: breakEffectAmt},
+		State:  dotBuffAmt,
+	})
+
+	// energy from killed enemy suffering owner's DOT
 }
 
 func buffDotOnUlt(mod *modifier.Instance, e event.ActionStart) {
