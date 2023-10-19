3cbf637d399a2b52a852a426c2b0ae5582616d50
interfacew <interface@163.com>
2023-10-19 19:37:55 +0800

    write stats & attack ratio

--- internal/character/jingliu/attack.go
+++ internal/character/jingliu/attack.go
@@ -5,6 +5,9 @@ import (
 	"github.com/simimpact/srsim/pkg/key"
 )
 
+// 1437+3353
+var attackRatio = []float64{0.3, 0.7}
+
 func (c *char) Attack(target key.TargetID, state info.ActionState) {
 
 }
--- internal/character/jingliu/skill.go
+++ internal/character/jingliu/skill.go
@@ -5,6 +5,9 @@ import (
 	"github.com/simimpact/srsim/pkg/key"
 )
 
+// 3*3616+7232+18080
+var EnhancedSkillRatio = []float64{0.1, 0.1, 0.1, 0.2, 0.5}
+
 func (c *char) Skill(target key.TargetID, state info.ActionState) {
-	
+
 }
