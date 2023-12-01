2d8feb516b625aeb7567425e0e70e51c8ea8ee70
Ryan Older <ryanolder67@gmail.com>
2023-12-01 14:15:08 +0700

    move A2 logic to ult.go

--- internal/character/welt/trace.go
+++ internal/character/welt/trace.go
@@ -3,48 +3,21 @@ package welt
 import (
 	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
-	"github.com/simimpact/srsim/pkg/engine/modifier"
 	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
 )
 
 const (
-	A2 key.Modifier = "welt-a2"
-	A4 key.Reason   = "welt-a4"
-	A6 key.Reason   = "welt-a6"
+	A4 key.Reason = "welt-a4"
+	A6 key.Reason = "welt-a6"
 )
 
 func init() {
-	modifier.Register(A2, modifier.Config{
-		Stacking:   modifier.ReplaceBySource,
-		StatusType: model.StatusType_STATUS_DEBUFF,
-	})
+
 }
 
 func (c *char) initTraces() {
-	// A2 : When using Ultimate, there is a 100% base chance to
-	// 			increase the DMG received by the targets by 12% for 2 turn(s).
-	// TODO : consider moving this logic to Ult.go
-	c.engine.Events().AttackStart.Subscribe(func(e event.AttackStart) {
-		// negative condition for early return
-		if e.Attacker != c.id ||
-			!c.info.Traces["101"] ||
-			e.AttackType != model.AttackType_ULT {
-			return
-		}
-		// inflict allDmgTaken debuff to all targets
-		for _, target := range e.Targets {
-			c.engine.AddModifier(target, info.Modifier{
-				Name:     A2,
-				Source:   c.id,
-				Chance:   1,
-				Duration: 2,
-				Stats:    info.PropMap{prop.AllDamageTaken: 0.12},
-			})
-		}
-	})
-
 	// A4 : Using Ultimate additionally regenerates 10 Energy.
 	c.engine.Events().ActionEnd.Subscribe(func(e event.ActionEnd) {
 		if e.Owner != c.id ||
--- internal/character/welt/ult.go
+++ internal/character/welt/ult.go
@@ -3,6 +3,8 @@ package welt
 import (
 	"github.com/simimpact/srsim/internal/global/common"
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/modifier"
+	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
 )
@@ -12,12 +14,34 @@ import (
 // Imprisoned enemies have their actions delayed by 40% and SPD reduced by 10%.
 
 const (
-	Ult key.Attack = "welt-ult"
+	Ult key.Attack   = "welt-ult"
+	A2  key.Modifier = "welt-a2"
 )
+
+func init() {
+	modifier.Register(A2, modifier.Config{
+		Stacking:   modifier.ReplaceBySource,
+		StatusType: model.StatusType_STATUS_DEBUFF,
+	})
+}
 
 var attackHits = []float64{0.1, 0.9}
 
 func (c *char) Ult(target key.TargetID, state info.ActionState) {
+	// A2 : When using Ultimate, there is a 100% base chance to
+	// 		increase the DMG received by the targets by 12% for 2 turn(s).
+	if c.info.Traces["101"] {
+		for _, target := range c.engine.Enemies() {
+			c.engine.AddModifier(target, info.Modifier{
+				Name:     A2,
+				Source:   c.id,
+				Chance:   1,
+				Duration: 2,
+				Stats:    info.PropMap{prop.AllDamageTaken: 0.12},
+			})
+		}
+	}
+
 	// global AOE attack
 	for _, target := range c.engine.Enemies() {
 		// multiple hits attack.
