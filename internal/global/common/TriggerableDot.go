9ce1dae7cff0c161df1995488143bd6ac46c9115
aaron-hwang <aaronhwang22@gmail.com>
2023-12-23 15:45:17 -0500

    Added comment

--- internal/global/common/TriggerableDot.go
+++ internal/global/common/TriggerableDot.go
@@ -3,5 +3,6 @@ package common
 import "github.com/simimpact/srsim/pkg/engine/modifier"
 
 type TriggerableDot interface {
+	//Trigger the corresponding DoT effect to deal damage
 	TriggerDot(mod *modifier.Instance, ratio float64)
 }
