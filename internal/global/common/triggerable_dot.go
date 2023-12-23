2dc79bacc389db5aeea48eb800458d5f4bbfb11b
aaron-hwang <aaronhwang22@gmail.com>
2023-12-23 18:34:44 -0500

    Renamed interface file

---
+++ internal/global/common/triggerable_dot.go
@@ -0,0 +1,8 @@
+package common
+
+import "github.com/simimpact/srsim/pkg/engine/modifier"
+
+type TriggerableDot interface {
+	// Trigger the corresponding DoT effect to deal damage
+	TriggerDot(mod *modifier.Instance, ratio float64)
+}
