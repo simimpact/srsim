b35d35fcb33b60633549338ecbf664225af8b1b5
aaron-hwang <aaronhwang22@gmail.com>
2024-01-16 01:04:10 -0500

    Talent implementaton

--- internal/character/kafka/talent.go
+++ internal/character/kafka/talent.go
@@ -3,37 +3,89 @@ package kafka
 import (
 	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/modifier"
+	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
 )
 
 const (
-	kafkaTalent   = "kafka-talent"
-	kafkaFollowup = "kafka-followup"
+	kafkaTalent = "kafka-talent"
+	followup    = "kafka-followup"
+	canAttack   = "kafka-talent-can-attack"
 )
 
+// I don't like this but couldn't get it working as just a variable in the char struct so
 func init() {
+	modifier.Register(kafkaTalent, modifier.Config{
+		Listeners: modifier.Listeners{
+			OnPhase2: restoreTalent,
+		},
+	})
 
+}
+
+func restoreTalent(mod *modifier.Instance) {
+	if !mod.Engine().HasModifier(mod.Owner(), canAttack) {
+		mod.Engine().AddModifier(mod.Owner(), info.Modifier{
+			Name:   canAttack,
+			Source: mod.Owner(),
+		})
+	}
 }
 
 func (c *char) initTalent() {
+	c.engine.AddModifier(c.id, info.Modifier{
+		Name:   kafkaTalent,
+		Source: c.id,
+	})
 	c.engine.Events().AttackEnd.Subscribe(c.talentTrigger)
+	c.engine.Events().ActionEnd.Subscribe(c.talentAttack)
 }
 
 var talentHitSplit = []float64{0.15, 0.15, 0.15, 0.15, 0.15, 0.25}
+var talentTargs = []key.TargetID{}
 
 func (c *char) talentTrigger(e event.AttackEnd) {
-	canAttack := c.engine.IsCharacter(e.Attacker) && e.AttackType == model.AttackType_NORMAL && e.Attacker != c.id && c.canUseTalent
+	isFriendly := c.engine.IsCharacter(e.Attacker)
+	isBasicAtk := e.AttackType == model.AttackType_NORMAL
+	isNotKafka := e.Attacker != c.id
+	canAttack := isFriendly && isBasicAtk && isNotKafka && c.engine.HasModifier(c.id, canAttack)
 	if canAttack {
+		talentTargs = e.Targets
+	}
+}
+
+func (c *char) talentAttack(e event.ActionEnd) {
+	isBasicAtk := e.AttackType == model.AttackType_NORMAL
+	target := talentTargs[0]
+
+	if c.info.Eidolon >= 1 {
+		c.engine.AddModifier(target, info.Modifier{
+			Name:     E1,
+			Source:   c.id,
+			Chance:   1,
+			Duration: 2,
+		})
+	}
+
+	if isBasicAtk && c.engine.HasModifier(c.id, canAttack) && c.engine.HPRatio(target) <= 0 {
 		c.engine.InsertAbility(info.Insert{
-			Key: kafkaTalent,
+			Key: followup,
 			Execute: func() {
 				for index, hit := range talentHitSplit {
 					c.engine.Attack(info.Attack{
-						Key:      kafkaFollowup,
-						HitIndex: index,
-						HitRatio: hit,
-						//
-						Targets: e.Targets,
+						Key:          followup,
+						HitIndex:     index,
+						HitRatio:     hit,
+						Source:       c.id,
+						Targets:      []key.TargetID{target},
+						EnergyGain:   10,
+						StanceDamage: 30,
+						DamageType:   model.DamageType_THUNDER,
+						AttackType:   model.AttackType_INSERT,
+						BaseDamage: info.DamageMap{
+							model.DamageFormula_BY_ATK: talent[c.info.TalentLevelIndex()],
+						},
 					})
 				}
 			},
@@ -42,4 +94,9 @@ func (c *char) talentTrigger(e event.AttackEnd) {
 			Priority:   info.CharInsertAttackSelf,
 		})
 	}
+
+	c.applyUltShock([]key.TargetID{target})
+
+	c.engine.RemoveModifier(c.id, canAttack)
+
 }
