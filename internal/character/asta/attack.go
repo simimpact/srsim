f7eead64cd58785f4c155ad2d21b5bccb1982725
ducc <aaronhwang22@gmail.com>
2024-02-04 00:03:45 -0500

    Skeleton

---
+++ internal/character/asta/attack.go
@@ -0,0 +1,14 @@
+package asta
+
+import (
+	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/key"
+)
+
+const (
+	A2 = "asta-a2"
+)
+
+func (c *char) Attack(target key.TargetID, state info.ActionState) {
+
+}
