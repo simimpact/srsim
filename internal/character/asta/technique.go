2cda532997d6b0c3634299ba86d9ccffdc93e216
ducc <aaronhwang22@gmail.com>
2024-02-24 23:57:07 -0500

    Technique implementation

--- internal/character/asta/technique.go
+++ internal/character/asta/technique.go
@@ -3,8 +3,22 @@ package asta
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
+)
+
+const (
+	asta_technique = "asta-technique"
 )
 
 func (c *char) Technique(target key.TargetID, state info.ActionState) {
-	
+	c.engine.Attack(info.Attack{
+		Key:        asta_technique,
+		Targets:    c.engine.Enemies(),
+		Source:     c.id,
+		AttackType: model.AttackType_MAZE,
+		DamageType: model.DamageType_FIRE,
+		BaseDamage: info.DamageMap{
+			model.DamageFormula_BY_ATK: 0.5,
+		},
+	})
 }
