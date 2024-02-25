3f9945c5750938cc61f20f38ad5257dda72d972d
ducc <aaronhwang22@gmail.com>
2024-02-24 21:04:11 -0500

    Added e2 and e4 modifiers

--- internal/character/asta/eidolon.go
+++ internal/character/asta/eidolon.go
@@ -1,1 +1,18 @@
 package asta
+
+import "github.com/simimpact/srsim/pkg/engine/modifier"
+
+const (
+	e2 = "asta-e2-flag"
+	e4 = "asta-e4"
+)
+
+func init() {
+	modifier.Register(e2, modifier.Config{
+		Duration: 1,
+	})
+
+	modifier.Register(e4, modifier.Config{
+		Stacking: modifier.ReplaceBySource,
+	})
+}
