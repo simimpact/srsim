93af14ae0002dad9154a586760de1aa2aaeb1d52
ducc <aaronhwang22@gmail.com>
2024-03-02 17:16:49 -0500

    E1, E2, E6

--- internal/character/herta/eidolon.go
+++ internal/character/herta/eidolon.go
@@ -1,9 +1,22 @@
 package herta
 
+import "github.com/simimpact/srsim/pkg/engine/modifier"
+
 const (
 	e1 = "herta-e1"
+	e2 = "herta-e2"
+	e6 = "herta-e6"
 )
 
 func init() {
+	modifier.Register(e2, modifier.Config{
+		Stacking:          modifier.ReplaceBySource,
+		CountAddWhenStack: 1,
+		Count:             1,
+	})
 
+	modifier.Register(e6, modifier.Config{
+		Stacking: modifier.ReplaceBySource,
+		Duration: 1,
+	})
 }
