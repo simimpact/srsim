00395c01cbe43c3dbde264d46cefe4b661c6f057
ducc <aaronhwang22@gmail.com>
2024-03-02 17:17:29 -0500

    Traces implemented

--- internal/character/herta/trace.go
+++ internal/character/herta/trace.go
@@ -1,13 +1,39 @@
 package herta
 
+import (
+	"github.com/simimpact/srsim/pkg/engine/event"
+	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/modifier"
+	"github.com/simimpact/srsim/pkg/engine/prop"
+	"github.com/simimpact/srsim/pkg/model"
+)
+
 const (
-	a2 = "herta-a2	"
+	a4 = "herta-a4"
+	a6 = "herta-a6"
 )
 
 func init() {
+	modifier.Register(a4, modifier.Config{})
 
+	modifier.Register(a6, modifier.Config{
+		Listeners: modifier.Listeners{
+			OnBeforeBeingHit: a6BeforeBeingHit,
+		},
+	})
 }
 
 func (c *char) initTraces() {
+	if c.info.Traces["102"] {
+		c.engine.AddModifier(c.id, info.Modifier{
+			DebuffRES: info.DebuffRESMap{
+				model.BehaviorFlag_STAT_CTRL: 0.35,
+			},
+		},
+		)
+	}
+}
 
+func a6BeforeBeingHit(mod *modifier.Instance, e event.HitStart) {
+	e.Hit.Attacker.AddProperty(a6, prop.AllDamagePercent, 0.2)
 }
