260b766bbb15eb0e24be21b6171e0f6a7036f8bb
ducc <aaronhwang22@gmail.com>
2024-03-02 17:17:20 -0500

    Technique

--- internal/character/herta/technique.go
+++ internal/character/herta/technique.go
@@ -2,9 +2,21 @@ package herta
 
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
 )
 
+const (
+	technique = "herta-technique"
+)
+
 func (c *char) Technique(target key.TargetID, state info.ActionState) {
-	
+	c.engine.AddModifier(c.id, info.Modifier{
+		Name:   technique,
+		Source: c.id,
+		Stats: info.PropMap{
+			prop.ATKPercent: 0.4,
+		},
+		Duration: 3,
+	})
 }
