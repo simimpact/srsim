d2b055cfc0d4ae0e74162f5c377bb970c624d28b
kdovtdc <kdovtdc@live.de>
2024-08-25 18:11:02 +0200

    Implement Tingyun Ult, E6, Technique

--- internal/character/tingyun/technique.go
+++ internal/character/tingyun/technique.go
@@ -5,6 +5,14 @@ import (
 	"github.com/simimpact/srsim/pkg/key"
 )
 
+const Technique key.Reason = "tingyun-technique"
+
 func (c *char) Technique(target key.TargetID, state info.ActionState) {
-	
+	// Technique can be used multiple times in succession to gain the Energy the same amount of times
+	c.engine.ModifyEnergy(info.ModifyAttribute{
+		Key:    Technique,
+		Target: c.id,
+		Source: c.id,
+		Amount: 50.0,
+	})
 }
