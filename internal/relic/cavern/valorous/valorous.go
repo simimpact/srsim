531fcfba270b6e604d63bc7be14c8dd59213c8db
clevernt <Azozy2011.hd@gmail.com>
2024-08-30 01:45:16 +0300

    valorous

---
+++ internal/relic/cavern/valorous/valorous.go
@@ -0,0 +1,71 @@
+package valorous
+
+import (
+	"github.com/simimpact/srsim/pkg/engine"
+	"github.com/simimpact/srsim/pkg/engine/equip/relic"
+	"github.com/simimpact/srsim/pkg/engine/event"
+	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/modifier"
+	"github.com/simimpact/srsim/pkg/engine/prop"
+	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
+)
+
+const (
+	name = "wind-soaring-valorous"
+)
+
+// 2pc: Increases ATK by 12%.
+// 4pc: Increases the wearer's CRIT Rate by 6%. 
+//		After the wearer uses follow-up attack, 
+//		increases DMG dealt by Ultimate by 36%, lasting for 1 turn(s).
+func init() {
+	relic.Register(key.WindSoaringValorous, relic.Config{
+		Effects: []relic.SetEffect{
+			{
+				MinCount: 2,
+				Stats:    info.PropMap{prop.ATKPercent: 0.12},
+			},
+			{
+				MinCount: 4,
+				Stats:	  info.PropMap{prop.CritChance: 0.06},
+				CreateEffect: func(engine engine.Engine, owner key.TargetID) {
+					engine.AddModifier(owner, info.Modifier{
+						Name:   mod,
+						Source: owner,
+					})
+				},
+			},
+		},
+	})
+
+	modifier.Register(mod, modifier.Config{
+		Listeners: modifier.Listeners{
+			OnBeforeAttack: onBeforeAttack,
+		},
+	})
+
+	modifier.Register(BuffUltDmg, modifier.Config{
+		Stacking:   modifier.ReplaceBySource,
+		StatusType: model.StatusType_STATUS_BUFF,
+		Listeners: modifier.Listeners{
+			OnBeforeHitAll: doBuffUlt,
+		}
+	})
+}
+
+func onBeforeAttack(mod *modifier.Instance, e event.AttackStart) {
+	if e.AttackType == model.AttackType_INSERT {
+		mod.Engine().AddModifier(mod.Owner(), info.Modifier{
+			Name:     BuffUltDmg,
+			Source:   mod.Owner(),
+			Duration: 1,
+		})
+	}
+}
+
+func doBuffUlt(mod *modifier.Instance, e event.BeforeHit)  {
+	if e.AttackType == model.AttackType_ULT {
+		e.Hit.Attacker.AddProperty(prop.AllDamagePercent, 0.36)
+	}
+}
