ee72d9567a15857c01fa6759a2022f30a53b6dbb
ducc <aaronhwang22@gmail.com>
2024-09-12 16:36:30 -0400

    Finished skill implementation

--- internal/character/luka/skill.go
+++ internal/character/luka/skill.go
@@ -1,11 +1,27 @@
 package luka
 
 import (
-	"github.com/simimpact/srsim/internal/global/common"
+	"github.com/simimpact/srsim/pkg/engine"
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/modifier"
 	"github.com/simimpact/srsim/pkg/key"
 	"github.com/simimpact/srsim/pkg/model"
 )
+
+const (
+	LukaBleed = "luka-bleed"
+)
+
+func init() {
+	modifier.Register(
+		LukaBleed,
+		modifier.Config{
+			Stacking:      modifier.ReplaceBySource,
+			Duration:      3,
+			BehaviorFlags: []model.BehaviorFlag{model.BehaviorFlag_STAT_DOT, model.BehaviorFlag_STAT_DOT_BLEED},
+		},
+	)
+}
 
 func (c *char) Skill(target key.TargetID, state info.ActionState) {
 	c.e1Check(target)
@@ -35,13 +51,46 @@ func (c *char) Skill(target key.TargetID, state info.ActionState) {
 
 	c.applyBleed(target)
 	c.incrementFightingSprit()
+	state.EndAttack()
+}
 
+// Luka's bleed has custom logic that differs slightly from common bleeds
+type LukaBleedState struct {
+	DamagePercentage    float64
+	EnemyHealthRatioCap float64
 }
 
 func (c *char) applyBleed(target key.TargetID) {
 	c.engine.AddModifier(target, info.Modifier{
-		Name:   common.Bleed,
+		Name:   LukaBleed,
 		Source: c.id,
-		State:  common.BleedState{},
+		State: LukaBleedState{
+			DamagePercentage:    skillDotCap[c.info.SkillLevelIndex()],
+			EnemyHealthRatioCap: 0.24,
+		},
+		Chance:   1,
+		Duration: 3,
 	})
+}
+
+// Implementation of: github.com/srsim/internal/global/common/triggerable_dot.go
+func (b *LukaBleedState) TriggerDot(dot info.Modifier, ratio float64, engine engine.Engine, target key.TargetID) {
+	owner := engine.Stats(dot.Source)
+	targetStats := engine.Stats(target)
+	bleedDamage := b.EnemyHealthRatioCap * targetStats.MaxHP()
+	skillCap := b.DamagePercentage * owner.ATK()
+	if bleedDamage > (skillCap) {
+		bleedDamage = skillCap
+	}
+	engine.Attack(
+		info.Attack{
+			Key:          LukaBleed,
+			Source:       dot.Source,
+			Targets:      []key.TargetID{target},
+			AttackType:   model.AttackType_DOT,
+			DamageType:   model.DamageType_PHYSICAL,
+			DamageValue:  bleedDamage,
+			AsPureDamage: true,
+		},
+	)
 }
