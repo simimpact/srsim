c73d15a89069acac94945ea57ff23c5f93b9c344
clevernt <Azozy2011.hd@gmail.com>
2024-09-25 17:04:10 +0300

    add StatusType

--- internal/relic/planar/lushaka/lushaka.go
+++ internal/relic/planar/lushaka/lushaka.go
@@ -8,6 +8,7 @@ import (
 	"github.com/simimpact/srsim/pkg/engine/modifier"
 	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
 )
 
 const (
@@ -29,7 +30,8 @@ func init() {
 	})
 
 	modifier.Register(name, modifier.Config{
-		Stacking: modifier.ReplaceBySource,
+		StatusType: model.StatusType_STATUS_BUFF,
+		Stacking:   modifier.ReplaceBySource,
 	})
 }
 
