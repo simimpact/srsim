49b6a7fc17fe4012adc56d2cf75ecfaccc1f4baf
kdovtdc <kdovtdc@live.de>
2024-10-05 19:31:32 +0200

    Implement Along the Passing Shore (LC)
    * init lc setup
    * register modifiers
    * added cdmg to owner
    * implemented logic for modifiers

--- pkg/simulation/imports.go
+++ pkg/simulation/imports.go
@@ -78,6 +78,7 @@ import (
 	_ "github.com/simimpact/srsim/internal/lightcone/hunt/sleeplikethedead"
 	_ "github.com/simimpact/srsim/internal/lightcone/hunt/subscribeformore"
 	_ "github.com/simimpact/srsim/internal/lightcone/hunt/swordplay"
+	_ "github.com/simimpact/srsim/internal/lightcone/nihility/alongthepassingshore"
 	_ "github.com/simimpact/srsim/internal/lightcone/nihility/beforethetutorialmissionstarts"
 	_ "github.com/simimpact/srsim/internal/lightcone/nihility/eyesoftheprey"
 	_ "github.com/simimpact/srsim/internal/lightcone/nihility/fermata"
