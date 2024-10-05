69cd996515ac35ab2a0a40ad4bbe26160386bbec
interfacew <interface@163.com>
2024-10-06 01:16:24 +0800

    correct listener type use mod.Source to track attacker

--- internal/character/trailblazerimaginary/superbreak.go
+++ internal/character/trailblazerimaginary/superbreak.go
@@ -1,6 +1,7 @@
 package trailblazerimaginary
 
 import (
+	"github.com/simimpact/srsim/pkg/engine"
 	"github.com/simimpact/srsim/pkg/engine/event"
 	"github.com/simimpact/srsim/pkg/engine/info"
 	"github.com/simimpact/srsim/pkg/engine/modifier"
@@ -31,7 +32,7 @@ func init() {
 		StatusType: model.StatusType_UNKNOWN_STATUS,
 		Stacking:   modifier.ReplaceBySource,
 		Listeners: modifier.Listeners{
-			OnBeforeHit: stanceDamageListener,
+			OnBeforeBeingHitAll: stanceDamageListener,
 		},
 	})
 }
@@ -40,7 +41,7 @@ func startStanceDamageRecord(mod *modifier.Instance, e event.AttackStart) {
 	for _, target := range mod.Engine().Enemies() {
 		mod.Engine().AddModifier(target, info.Modifier{
 			Name:   StanceDamageRecorder,
-			Source: mod.Source(),
+			Source: mod.Owner(),
 			State: &recorderData{
 				stanceDamage: 0.0,
 			},
@@ -49,23 +50,39 @@ func startStanceDamageRecord(mod *modifier.Instance, e event.AttackStart) {
 }
 
 func stanceDamageListener(mod *modifier.Instance, e event.HitStart) {
+	if e.Attacker != mod.Source() {
+		return
+	}
 	if mod.Engine().Stance(e.Defender) == 0 {
 		mod.State().(*recorderData).stanceDamage += e.Hit.StanceDamage
 	}
+}
+
+func getModifierFromSource(engine engine.Engine, target key.TargetID, source key.TargetID, modifier key.Modifier) []info.Modifier {
+	modifierList := engine.GetModifiers(target, modifier)
+	resList := []info.Modifier{}
+	for _, mod := range modifierList {
+		if mod.Source == source {
+			resList = append(resList, mod)
+		}
+	}
+	return resList
 }
 
 var vulner = []float64{0, 0.6, 0.5, 0.4, 0.3, 0.2}
 
 func triggerSuperBreak(mod *modifier.Instance, e event.AttackEnd) {
 	for _, target := range mod.Engine().Enemies() {
-		stanceDamage := mod.Engine().GetModifiers(target, StanceDamageRecorder)[0].State.(recorderData).stanceDamage
-		mod.Engine().RemoveModifier(target, StanceDamageRecorder)
+		recorder := getModifierFromSource(mod.Engine(), target, mod.Owner(), StanceDamageRecorder)[0]
+		stanceDamage := recorder.State.(*recorderData).stanceDamage
+		mod.Engine().RemoveModifierFromSource(target, mod.Owner(), StanceDamageRecorder)
+
 		if stanceDamage == 0 {
 			continue
 		}
 
 		sourceInfo, _ := mod.Engine().CharacterInfo(mod.Source())
-		attackerInfo, _ := mod.Engine().CharacterInfo(e.Attacker)
+		attackerInfo, _ := mod.Engine().CharacterInfo(mod.Owner())
 		attackerStats := mod.Engine().Stats(e.Attacker)
 
 		damage := baseBreakDamage[attackerInfo.Level] * (1 + attackerStats.BreakEffect()) * stanceDamage
@@ -80,7 +97,7 @@ func triggerSuperBreak(mod *modifier.Instance, e event.AttackEnd) {
 
 		mod.Engine().Attack(info.Attack{
 			Key:          SuperBreak,
-			Source:       e.Attacker,
+			Source:       mod.Owner(),
 			Targets:      []key.TargetID{target},
 			DamageType:   attackerInfo.Element,
 			AttackType:   model.AttackType_ELEMENT_DAMAGE,
