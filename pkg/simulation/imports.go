e3a3d55d48f7c16fd702ee1fb7f0686a1881c0bf
metalcupcake5 <43897385+metalcupcake5@users.noreply.github.com>
2024-10-22 15:55:05 -0400

    duke (#279)
    * duke
    
    * fix typo
    
    * update duke id
    
    * update duke
    
    * set buff
    
    * lint duke.go
    
    * change buff multiplier on stack
    
    * add stats field to duke effects

--- pkg/simulation/imports.go
+++ pkg/simulation/imports.go
@@ -104,6 +104,7 @@ import (
 	_ "github.com/simimpact/srsim/internal/lightcone/preservation/trendoftheuniversalmarket"
 	_ "github.com/simimpact/srsim/internal/lightcone/preservation/wearewildfire"
 	_ "github.com/simimpact/srsim/internal/relic/cavern/championboxing"
+	_ "github.com/simimpact/srsim/internal/relic/cavern/duke"
 	_ "github.com/simimpact/srsim/internal/relic/cavern/genius"
 	_ "github.com/simimpact/srsim/internal/relic/cavern/hunterofglacialforest"
 	_ "github.com/simimpact/srsim/internal/relic/cavern/musketeer"
