4c35759d914375c3d2cfd71476c60806f5faeb2a
srl <906239+srliao@users.noreply.github.com>
2024-10-25 08:51:10 -0400

    ui features (#317)
    * add hit info to hit end
    
    * change tygo to use custom type mapping
    
    * add gauge change log
    
    * clean up logcard + turnorder
    
    * make clickcable color blue
    
    * remove unused script

--- js/apps/web/src/app/viewer/log/TurnStart.tsx
+++ js/apps/web/src/app/viewer/log/TurnStart.tsx
@@ -2,6 +2,8 @@ import { event } from "@srsim/ts-types";
 import React from "react";
 import { LogCard } from "./LogCard";
 import { FcSportsMode } from "react-icons/fc";
+import { JSONTree } from "react-json-tree";
+import { TurnOrder } from "./TurnOrder";
 
 type Prop = {
   names: { [key: string]: string };
@@ -18,6 +20,8 @@ export const TurnStart = ({ names, event }: Prop) => {
       className="pt-6"
       msg={`${names[e.event.active] ?? e.event.active}'s turn (current av: ${e.event.total_av.toFixed(0)})`}
       icon={<FcSportsMode />}
-    />
+    >
+      <TurnOrder names={names} turns={e.event.turn_order} />
+    </LogCard>
   );
 };
