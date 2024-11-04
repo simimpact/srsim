35f8a87add5d88a5905465cda2e492d73f17f600
ducc <aaronhwang22@gmail.com>
2024-11-04 16:17:07 -0500

    gallagher skill impl

--- internal/character/gallagher/skill.go
+++ internal/character/gallagher/skill.go
@@ -2,9 +2,35 @@ package gallagher
 
 import (
 	"github.com/simimpact/srsim/pkg/engine/info"
+	"github.com/simimpact/srsim/pkg/engine/prop"
 	"github.com/simimpact/srsim/pkg/key"
+	"github.com/simimpact/srsim/pkg/model"
+)
+
+const (
+	Skill key.Heal = "gallagher-skill"
 )
 
 func (c *char) Skill(target key.TargetID, state info.ActionState) {
-	
+	if c.info.Eidolon >= 2 {
+		c.engine.DispelStatus(target, info.Dispel{
+			Status: model.StatusType_STATUS_DEBUFF,
+			Order:  model.DispelOrder_LAST_ADDED,
+			Count:  1,
+		})
+		c.engine.AddModifier(target, info.Modifier{
+			Name: E2,
+			Stats: info.PropMap{
+				prop.EffectRES: 0.3,
+			},
+			Duration: 2,
+		})
+	}
+
+	c.engine.Heal(info.Heal{
+		Key:       Skill,
+		Source:    c.id,
+		Targets:   []key.TargetID{target},
+		HealValue: skill[c.info.SkillLevelIndex()],
+	})
 }
