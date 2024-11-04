9bd8cea281fe0078295b93a43701594656efaf21
ducc <aaronhwang22@gmail.com>
2024-11-04 00:14:09 -0500

    Added missing behavior flags, changed spd bufff to spd convert

--- internal/character/hanya/ult.go
+++ internal/character/hanya/ult.go
@@ -14,8 +14,9 @@ const (
 
 func init() {
 	modifier.Register(Ultimate, modifier.Config{
-		StatusType: model.StatusType_STATUS_BUFF,
-		Stacking:   modifier.Replace,
+		StatusType:    model.StatusType_STATUS_BUFF,
+		Stacking:      modifier.Replace,
+		BehaviorFlags: []model.BehaviorFlag{model.BehaviorFlag_STAT_SPEED_UP},
 	})
 }
 
@@ -24,14 +25,14 @@ func (c *char) Ult(target key.TargetID, state info.ActionState) {
 	if c.info.Eidolon >= 4 {
 		ultdur = 3
 	}
-
+	hanyaStats := c.engine.Stats(c.id)
 	c.engine.AddModifier(target, info.Modifier{
 		Name:     Ultimate,
 		Source:   c.id,
 		Duration: ultdur,
 		Stats: info.PropMap{
 			prop.ATKPercent: ultAtkBuff[c.info.UltLevelIndex()],
-			prop.SPDFlat:    ultSpdBuff[c.info.UltLevelIndex()] * c.engine.Stats(c.id).SPD(),
+			prop.SPDConvert: ultSpdBuff[c.info.UltLevelIndex()]*hanyaStats.SPD() - hanyaStats.GetProperty(prop.SPDConvert),
 		},
 	})
 
