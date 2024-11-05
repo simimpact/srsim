e649b411c6c827552a1bb19dd7517033069ade46
ducc <aaronhwang22@gmail.com>
2024-11-05 15:45:07 -0500

    technique impl

--- internal/character/gallagher/technique.go
+++ internal/character/gallagher/technique.go
@@ -6,5 +6,16 @@ import (
 )
 
 func (c *char) Technique(target key.TargetID, state info.ActionState) {
-	
+	for _, enemy := range c.engine.Enemies() {
+		c.engine.AddModifier(enemy, info.Modifier{
+			Name: Besotted,
+			State: &BesottedState{
+				a6Active:  c.info.Traces["103"],
+				breakVuln: talent[c.info.TalentLevelIndex()],
+				healAmt:   talent_heal[c.info.TalentLevelIndex()],
+			},
+			// Technique is always duration of 2
+			Duration: 2,
+		})
+	}
 }
