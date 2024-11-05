6d2ef1e1709a622727af84887debfef06011c44c
ducc <aaronhwang22@gmail.com>
2024-11-05 14:53:35 -0500

    Xueyi (#326)
    * initializaton
    
    * Beginning of talent implementation
    
    * A2 implementation
    
    * Added support for tracking karma stacks
    
    * E4 Implementation
    
    * Added ult buff scaling
    
    * Added A4 Implementation
    
    * Added ult implementation
    
    * Added E1, E2
    
    * Added Trace checks
    
    * Most/All of talent implementation
    
    * Implemented technique
    
    * Unwasted variable assignment
    
    * Variable assignment cleanup
    
    * Added talent and trace initialization
    
    * Linting
    
    * Fixed naming of E1 callback
    
    * Fixed duplicate key check
    
    * Fixed negative val

--- pkg/simulation/imports.go
+++ pkg/simulation/imports.go
@@ -27,6 +27,7 @@ import (
 	_ "github.com/simimpact/srsim/internal/character/serval"
 	_ "github.com/simimpact/srsim/internal/character/silverwolf"
 	_ "github.com/simimpact/srsim/internal/character/sushang"
+	_ "github.com/simimpact/srsim/internal/character/xueyi"
 	_ "github.com/simimpact/srsim/internal/enemy/dummy"
 	_ "github.com/simimpact/srsim/internal/global"
 	_ "github.com/simimpact/srsim/internal/lightcone/abundance/cornucopia"
